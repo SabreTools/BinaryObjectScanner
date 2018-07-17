8723639ebcfbe7a218904355147fcd16775c11ca
Matt Nadareski <mnadareski@outlook.com>
2018-07-16 17:10:45 -0700

    Add simple program for functionality testing

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using BurnOutSharp;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(String.Join("\r\n", ProtectionFind.Scan(arg).Select(kvp => kvp.Key + ": " + kvp.Value)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
