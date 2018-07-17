99d4adee75d26785e2280d91d6898d0f1cdb69c3
Matt Nadareski <mnadareski@outlook.com>
2018-07-17 10:25:41 -0700

    Update version

--- BurnOutSharp/Properties/AssemblyInfo.cs
+++ BurnOutSharp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BurnOutSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Port of BurnOut to C#, with additions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Matt Nadareski, Gernot Knippen")]
 [assembly: AssemblyProduct("BurnOutSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright (c)2005-2010 Gernot Knippen, Copyright (c)2018 Matt Nadareski")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.03.6")]
+[assembly: AssemblyFileVersion("1.03.6.0")]
