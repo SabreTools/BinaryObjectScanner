43cbafc0f5ff3a3e20d6a27f0abde86b8439cbcf
Matt Nadareski <mnadareski@outlook.com>
2020-10-26 20:34:47 -0700

    Add better detection for XCP2

--- BurnOutSharp/ProtectionType/XCP.cs
+++ BurnOutSharp/ProtectionType/XCP.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using BurnOutSharp.FileType;
 
 namespace BurnOutSharp.ProtectionType
 {
@@ -11,9 +12,14 @@ namespace BurnOutSharp.ProtectionType
         {
             if (isDirectory)
             {
+                // INI-like file that can be parsed out
+                string xcpDatPath = files.FirstOrDefault(f => Path.GetFileName(f).Equals("VERSION.DAT", StringComparison.OrdinalIgnoreCase))
+                    ?? files.FirstOrDefault(f => Path.GetFileName(f).Equals("XCP.DAT", StringComparison.OrdinalIgnoreCase));
+                if (!string.IsNullOrWhiteSpace(xcpDatPath))
+                    return GetXCPVersion(xcpDatPath);
+
                 // TODO: Verify if these are OR or AND
-                if (files.Any(f => Path.GetFileName(f).Equals("XCP.DAT", StringComparison.OrdinalIgnoreCase))
-                    || files.Any(f => Path.GetFileName(f).Equals("ECDPlayerControl.ocx", StringComparison.OrdinalIgnoreCase))
+                if (files.Any(f => Path.GetFileName(f).Equals("ECDPlayerControl.ocx", StringComparison.OrdinalIgnoreCase))
                     || files.Any(f => Path.GetFileName(f).Equals("go.exe", StringComparison.OrdinalIgnoreCase))) // Path.Combine("contents", "go.exe")
                 {
                     return "XCP";
@@ -21,8 +27,14 @@ namespace BurnOutSharp.ProtectionType
             }
             else
             {
-                if (Path.GetFileName(path).Equals("XCP.DAT", StringComparison.OrdinalIgnoreCase)
-                    || Path.GetFileName(path).Equals("ECDPlayerControl.ocx", StringComparison.OrdinalIgnoreCase)
+                // INI-like file that can be parsed out
+                if (Path.GetFileName(path).Equals("VERSION.DAT", StringComparison.OrdinalIgnoreCase)
+                    || Path.GetFileName(path).Equals("XCP.DAT", StringComparison.OrdinalIgnoreCase))
+                {
+                    return GetXCPVersion(path);
+                }
+
+                if (Path.GetFileName(path).Equals("ECDPlayerControl.ocx", StringComparison.OrdinalIgnoreCase)
                     || Path.GetFileName(path).Equals("go.exe", StringComparison.OrdinalIgnoreCase))
                 {
                     return "XCP";
@@ -30,6 +42,19 @@ namespace BurnOutSharp.ProtectionType
             }
 
             return null;
+        }
+
+        private static string GetXCPVersion(string path)
+        {
+            try
+            {
+                var xcpIni = new IniFile(path);
+                return xcpIni["XCP.VERSION"];
+            }
+            catch
+            {
+               return null;
+            }
         }
     }
 }
