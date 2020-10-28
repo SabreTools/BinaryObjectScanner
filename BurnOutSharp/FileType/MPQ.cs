9802840309294a0b0893a671c38c94050b7910d1
Matt Nadareski <mnadareski@outlook.com>
2020-10-28 12:20:36 -0700

    Scan whole folder for MPQ

--- BurnOutSharp/FileType/MPQ.cs
+++ BurnOutSharp/FileType/MPQ.cs
@@ -48,23 +48,13 @@ namespace BurnOutSharp.FileType
                             string tempFile = Path.Combine(tempPath, sub);
                             Directory.CreateDirectory(Path.GetDirectoryName(tempFile));
                             mpqArchive.ExtractFile(sub, tempFile);
-
-                            // Collect and format all found protections
-                            var fileProtections = ProtectionFind.Scan(tempFile, includePosition);
-                            string protection = string.Join("\r\n", fileProtections.Select(kvp => kvp.Key + ": " + kvp.Value.TrimEnd()));
-
-                            // If tempfile cleanup fails
-                            try
-                            {
-                                File.Delete(tempFile);
-                            }
-                            catch { }
-
-                            if (!string.IsNullOrEmpty(protection))
-                                protections.Add($"\r\n{sub} - {protection}");
                         }
                         catch { }
                     }
+
+                    // Collect and format all found protections
+                    var fileProtections = ProtectionFind.Scan(tempPath, includePosition);
+                    protections = fileProtections.Select(kvp => kvp.Key + ": " + kvp.Value.TrimEnd()).ToList();
 
                     // If temp directory cleanup fails
                     try
