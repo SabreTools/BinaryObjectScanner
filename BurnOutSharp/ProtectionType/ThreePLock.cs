7f5e93db951d8ed85c81bfe5d063dc54604d4a2e
Matt Nadareski <mnadareski@outlook.com>
2020-10-30 17:02:58 -0700

    Fix 3PLock scanning finally

--- BurnOutSharp/ProtectionType/ThreePLock.cs
+++ BurnOutSharp/ProtectionType/ThreePLock.cs
@@ -4,20 +4,20 @@
     {
         public static string CheckContents(byte[] fileContent, bool includePosition = false)
         {
-            // ".ldr"
+            // .ldr
             byte[] check = new byte[] { 0x2E, 0x6C, 0x64, 0x72 };
             if (fileContent.Contains(check, out int position))
-                return "3PLock" + (includePosition ? $" (Index {position})" : string.Empty);
-
-            // ".ldt"
-            check = new byte[] { 0x2E, 0x6C, 0x64, 0x74 };
-            if (fileContent.Contains(check, out position))
-                return "3PLock" + (includePosition ? $" (Index {position})" : string.Empty);
+            {
+                // .ldt
+                byte[] check2 = new byte[] { 0x2E, 0x6C, 0x64, 0x74 };
+                if (fileContent.Contains(check2, out int position2))
+                    return "3PLock" + (includePosition ? $" (Index {position}, {position2})" : string.Empty);
+            }
 
             // "Y" + (char)0xC3 + "U" + (char)0x8B + (char)0xEC + (char)0x83 + (char)0xEC + "0SVW"
-            // check = new byte[] { 0x59, 0xC3, 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x30, 0x53, 0x56, 0x57 };
-            //if (fileContent.Contains(check, out position))
-            //    return "3PLock" + (includePosition ? $" (Index {position})" : string.Empty);
+            check = new byte[] { 0x59, 0xC3, 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x30, 0x53, 0x56, 0x57 };
+            if (fileContent.Contains(check, out position))
+                return "3PLock" + (includePosition ? $" (Index {position})" : string.Empty);
 
             return null;
         }
