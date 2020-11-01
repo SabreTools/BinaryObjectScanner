cff9582bf5d1911288debb7e6bf969bb4c687e2a
Matt Nadareski <mnadareski@outlook.com>
2020-11-01 14:30:32 -0800

    Comment out one of the 3PLock checks

--- BurnOutSharp/ProtectionType/ThreePLock.cs
+++ BurnOutSharp/ProtectionType/ThreePLock.cs
@@ -14,10 +14,11 @@
                     return "3PLock" + (includePosition ? $" (Index {position}, {position2})" : string.Empty);
             }
 
+            // This produced false positives in some DirectX 9.0c installer files
             // "Y" + (char)0xC3 + "U" + (char)0x8B + (char)0xEC + (char)0x83 + (char)0xEC + "0SVW"
-            check = new byte[] { 0x59, 0xC3, 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x30, 0x53, 0x56, 0x57 };
-            if (fileContent.Contains(check, out position))
-                return "3PLock" + (includePosition ? $" (Index {position})" : string.Empty);
+            //check = new byte[] { 0x59, 0xC3, 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x30, 0x53, 0x56, 0x57 };
+            //if (fileContent.Contains(check, out position))
+            //    return "3PLock" + (includePosition ? $" (Index {position})" : string.Empty);
 
             return null;
         }
