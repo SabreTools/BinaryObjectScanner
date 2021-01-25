267be3543c52afaf7369a078d7c529430c7c95e5
SilasLaspada <silasqwerty@ymail.com>
2021-01-25 00:49:26 -0700

    Add BD+ detection
    Detect BD+ based on the presence of a folder named "BDSVM".

---
+++ BurnOutSharp/ProtectionType/BDPlus.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BurnOutSharp.ProtectionType
+{
+    public class BDPlus
+    {
+		// TODO: Detect version
+        public static string CheckPath(string path, IEnumerable<string> files, bool isDirectory)
+        {
+            if (!isDirectory)
+                return null;
+
+            if (Directory.Exists(Path.Combine(path, "BDSVM")))
+            {
+                return "BD+";
+            }
+
+            return null;
+        }
+    }
+}
