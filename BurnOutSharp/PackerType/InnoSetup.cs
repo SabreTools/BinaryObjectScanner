54465ff4e75ee66da2df134f937d7f39731c48f2
Matt Nadareski <mnadareski@outlook.com>
2021-02-26 09:34:07 -0800

    Move both installers to packers

--- BurnOutSharp/PackerType/InnoSetup.cs
+++ BurnOutSharp/PackerType/InnoSetup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace BurnOutSharp.ProtectionType
+namespace BurnOutSharp.PackerType
 {
     public class InnoSetup : IContentCheck, IScannable
     {
--- BurnOutSharp/PackerType/WiseInstaller.cs
+++ BurnOutSharp/PackerType/WiseInstaller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Wise = WiseUnpacker.WiseUnpacker;
 
-namespace BurnOutSharp.ProtectionType
+namespace BurnOutSharp.PackerType
 {
     public class WiseInstaller : IContentCheck, IScannable
     {
