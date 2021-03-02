ac0e5e95a9628e000ffb65c4202391a83fcf4c06
Matt Nadareski <mnadareski@outlook.com>
2021-03-02 13:34:15 -0800

    Add note to RPT scan

--- BurnOutSharp/ProtectionType/RingPROTECH.cs
+++ BurnOutSharp/ProtectionType/RingPROTECH.cs
@@ -3,12 +3,13 @@
     public class RingPROTECH : IContentCheck
     {
         /// <inheritdoc/>
+        /// TODO: Investigate as this may be over-matching
         public string CheckContents(string file, byte[] fileContent, bool includePosition = false)
         {
             // (char)0x00 + "Allocator" + (char)0x00 + (char)0x00 + (char)0x00 + (char)0x00
             byte[] check = new byte[] { 0x00, 0x41, 0x6C, 0x6C, 0x6F, 0x63, 0x61, 0x74, 0x6F, 0x72, 0x00, 0x00, 0x00, 0x00 };
             if (fileContent.Contains(check, out int position))
-                return "Ring PROTECH" + (includePosition ? $" (Index {position})" : string.Empty);
+                return "Ring PROTECH [Check disc for physical ring]" + (includePosition ? $" (Index {position})" : string.Empty);
 
             return null;
         }
