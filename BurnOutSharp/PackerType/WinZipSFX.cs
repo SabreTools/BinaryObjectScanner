f058f7928c055e3a46bd36a10791bb225c2dd1f4
SilasLaspada <silasqwerty@ymail.com>
2021-03-02 16:43:42 -0700

    Add inital check for WinZip SFX archives
    Every version of WinZip SFX has the string "WinZip Self-Extractor" in it,

---
+++ BurnOutSharp/PackerType/WinZipSFX.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BurnOutSharp.PackerType
+{
+    public class WinZipSFX : IContentCheck, IScannable
+    {
+        /// <inheritdoc/>
+        public bool ShouldScan(byte[] magic) => true;
+
+        /// <inheritdoc/>
+        public string CheckContents(string file, byte[] fileContent, bool includePosition = false)
+        {
+            // "WinZip Self-Extractor"
+            byte[] check = new byte[] { 0x57, 0x69, 0x6E, 0x5A, 0x69, 0x70, 0x20, 0x53, 0x65, 0x6C, 0x66, 0x2D, 0x45, 0x78, 0x74, 0x72, 0x61, 0x63, 0x74, 0x6F, 0x72 };
+            if (fileContent.Contains(check, out int position))
+                return "WinZip SFX" + (includePosition ? $" (Index {position})" : string.Empty);
+
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public Dictionary<string, List<string>> Scan(Scanner scanner, string file)
+        {
+            if (!File.Exists(file))
+                return null;
+
+            using (var fs = File.OpenRead(file))
+            {
+                return Scan(scanner, fs, file);
+            }
+        }
+        /// <inheritdoc/>
+        public Dictionary<string, List<string>> Scan(Scanner scanner, Stream stream, string file)
+        {
+            return null;
+        }
+    }
+}
