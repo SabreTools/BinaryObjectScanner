98be2618f67a4e0d382e50fdb2d20908bbecf5b5
SilasLaspada <silasqwerty@ymail.com>
2021-03-02 17:13:20 -0700

    Add basic version detection
    Versions 3+ and 2.x are identified radically differently, so make separate methods for them.

--- BurnOutSharp/PackerType/WinZipSFX.cs
+++ BurnOutSharp/PackerType/WinZipSFX.cs
@@ -15,7 +15,14 @@ namespace BurnOutSharp.PackerType
             // "WinZip Self-Extractor"
             byte[] check = new byte[] { 0x57, 0x69, 0x6E, 0x5A, 0x69, 0x70, 0x20, 0x53, 0x65, 0x6C, 0x66, 0x2D, 0x45, 0x78, 0x74, 0x72, 0x61, 0x63, 0x74, 0x6F, 0x72 };
             if (fileContent.Contains(check, out int position))
-                return "WinZip SFX" + (includePosition ? $" (Index {position})" : string.Empty);
+            {
+                // "version="
+                byte[] check2 = new byte[] { 0x76, 0x65, 0x72, 0x73, 0x69, 0x6F, 0x6E, 0x3D };
+                if (fileContent.Contains(check2, out int position2))
+                    return $"WinZip SFX {GetV3PlusVersion(fileContent, position)}" + (includePosition ? $" (Index {position})" : string.Empty);
+                else
+                    return $"WinZip SFX {GetV2Version(fileContent, position)}" + (includePosition ? $" (Index {position})" : string.Empty);
+            }
 
             return null;
         }
@@ -35,6 +42,18 @@ namespace BurnOutSharp.PackerType
         public Dictionary<string, List<string>> Scan(Scanner scanner, Stream stream, string file)
         {
             return null;
+        }
+
+        private static string GetV3PlusVersion(byte[] fileContent, int position)
+        {
+            string version = "Version 3+";
+            return $"{version}";
+        }
+
+        private static string GetV2Version(byte[] fileContent, int position)
+        {
+            string version = "Version 2.x";
+            return $"{version}";
         }
     }
 }
