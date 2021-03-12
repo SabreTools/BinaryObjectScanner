381bf0333f73c5ec685659e9ed011f6883442a7b
SilasLaspada <silasqwerty@ymail.com>
2021-03-11 22:08:25 -0700

    Improve version 3+ detection
    Use an XML string to determine the version.

--- BurnOutSharp/PackerType/WinZipSFX.cs
+++ BurnOutSharp/PackerType/WinZipSFX.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
 
 namespace BurnOutSharp.PackerType
 {
@@ -16,13 +19,14 @@ namespace BurnOutSharp.PackerType
             // TODO: Harden detection to prevent a text file containing "version=" in a 2.x archive from being falsely detected as 3+ archive
 
             // "WinZip Self-Extractor"
+
             byte[] check = new byte[] { 0x57, 0x69, 0x6E, 0x5A, 0x69, 0x70, 0x20, 0x53, 0x65, 0x6C, 0x66, 0x2D, 0x45, 0x78, 0x74, 0x72, 0x61, 0x63, 0x74, 0x6F, 0x72 };
             if (fileContent.Contains(check, out int position))
             {
-                // "version="
-                byte[] check2 = new byte[] { 0x76, 0x65, 0x72, 0x73, 0x69, 0x6F, 0x6E, 0x3D };
+                // "<?xml"
+                byte[] check2 = new byte[] { 0x3C, 0x3F, 0x78, 0x6D, 0x6C };
                 if (fileContent.Contains(check2, out int position2))
-                    return $"WinZip SFX {GetV3PlusVersion(fileContent, position)}" + (includePosition ? $" (Index {position})" : string.Empty);
+                    return $"WinZip SFX {GetV3PlusVersion(fileContent, position, position2)}" + (includePosition ? $" (Index {position})" : string.Empty);
                 else
                     return $"WinZip SFX {GetV2Version(fileContent, position)}" + (includePosition ? $" (Index {position})" : string.Empty);
             }
@@ -47,7 +51,7 @@ namespace BurnOutSharp.PackerType
             return null;
         }
 
-        private static string GetV3PlusVersion(byte[] fileContent, int position)
+        private static string GetV3PlusVersion(byte[] fileContent, int position, int position2)
         {
             /* Every version above 3 contains an XML with a version string. Sometimes, the string doesnt exactly match the reported version number, but the internal version string can always be used to map to the public version number.
                Some generic version 3+ detection is possible (find the relevant "version=" string and stop at the ending quote), but every official 3+ is detected already, and with the version string not being reliable on it's own, this may not be worth implementing.
@@ -60,8 +64,36 @@ namespace BurnOutSharp.PackerType
                "W.i.n.Z.i.p. .S.e.l.f.-.E.x.t.r.a.c.t.o.r." (Found in every 3+ version aside from 3.0.7158. This appears to be text for messages that WinZip SFX might display to a user, so much more text like it exists if an even more specfic check is needed for some reason.)
                Hex: 0x57, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x5A, 0x00, 0x69, 0x00, 0x70, 0x00, 0x20, 0x00, 0x53, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x66, 0x00, 0x2D, 0x00, 0x45, 0x00, 0x78, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x63, 0x00, 0x74, 0x00, 0x6F, 0x00, 0x72, 0x00 */
 
-            // "3.0.7158.0"
-            byte[] check = new byte[] { 0x33, 0x2E, 0x30, 0x2E, 0x37, 0x31, 0x35, 0x38, 0x2E, 0x30 };
+            // Find the end of the XML so it can be imported and parsed
+            // </assembly>
+            byte[] check = new byte[] { 0x3C, 0x2F, 0x61, 0x73, 0x73, 0x65, 0x6D, 0x62, 0x6C, 0x79, 0x3E };
+            if (fileContent.Contains(check, out int position3))
+            {
+                int offset = position3 + 11 - position2;
+                string XML = Encoding.ASCII.GetString(fileContent, position2, offset);
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(XML);
+                string xmlVersion = xmlDoc["assembly"]["assemblyIdentity"].GetAttributeNode("version").InnerXml;
+                switch (xmlVersion)
+                {
+                    case "3.0.7158.0":
+                        return "3.0.7158";
+                    case "3.1.7556.0":
+                        return "3.1.7556";
+                    case "3.1.8421.0":
+                        return "4.0.8421";
+                    case "3.1.8672.0":
+                        return "4.0.8672";
+                    case "4.0.1221.0":
+                        return "4.0.12218";
+                    default:
+                        return $"(Unknown Version - {xmlVersion})";
+                }
+
+            }
+
+            /* // "3.0.7158.0"
+            check = new byte[] { 0x33, 0x2E, 0x30, 0x2E, 0x37, 0x31, 0x35, 0x38, 0x2E, 0x30 };
             if (fileContent.Contains(check, out position))
                 return "3.0.7158";
 
@@ -84,9 +116,8 @@ namespace BurnOutSharp.PackerType
             check = new byte[] { 0x34, 0x2E, 0x30, 0x2E, 0x31, 0x32, 0x32, 0x31, 0x2E, 0x30 };
             if (fileContent.Contains(check, out position))
                 return "4.0.12218";
-
-            else
-                return "Unknown version above 3.0";
+            */
+            return "Unknown version above 3.0";
         }
 
         private static string GetV2Version(byte[] fileContent, int position)
