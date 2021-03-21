4334acf73fe38b4feafdae4798e828629b1bd169
SilasLaspada <silasqwerty@ymail.com>
2021-03-20 21:52:16 -0600

    Address comments

--- BurnOutSharp/PackerType/PECompact.cs
+++ BurnOutSharp/PackerType/PECompact.cs
@@ -8,23 +8,22 @@ namespace BurnOutSharp.PackerType
         /// <inheritdoc/>
         public string CheckContents(string file, byte[] fileContent, bool includePosition = false)
         {
-            // "PEC2"
-            byte[] check = new byte[] { 0x50, 0x45, 0x43, 0x32 };
-            if (fileContent.FirstPosition(check, out int position, end: 2048))
-            {
-                string version = GetVersion(file, position);
-                if (version == null)
-                    return "PE Compact 2" + (includePosition ? $" (Index {position})" : string.Empty);
-                return $"PE Compact 2 Version {version}" + (includePosition ? $" (Index {position})" : string.Empty);
-            }
-
             // Another possible version string for version 1 is "PECO" (50 45 43 4F)
 
             // "pec1"
-            check = new byte[] { 0x70, 0x65, 0x63, 0x31 };
+            byte[] check = new byte[] { 0x70, 0x65, 0x63, 0x31 };
+            if (fileContent.FirstPosition(check, out int position, end: 2048))
+                return "PE Compact 1" + (includePosition ? $" (Index {position})" : string.Empty);
+
+            // "PEC2"
+            check = new byte[] { 0x50, 0x45, 0x43, 0x32 };
             if (fileContent.FirstPosition(check, out position, end: 2048))
             {
-                return "PE Compact 1" + (includePosition ? $" (Index {position})" : string.Empty);
+                string version = GetVersion(fileContent, position);
+                if (version == null)
+                    return "PE Compact 2" + (includePosition ? $" (Index {position})" : string.Empty);
+
+                return $"PE Compact 2 v{version}" + (includePosition ? $" (Index {position})" : string.Empty);
             }
 
             // "PECompact2"
@@ -35,25 +34,12 @@ namespace BurnOutSharp.PackerType
             return null;
         }
 
-        private static string GetVersion(string path, int position)
+        private static string GetVersion(byte[] fileContent, int position)
         {
-            if (!File.Exists(path))
-                return null;
-            try
-            {
-                using (var fs = File.OpenRead(path))
-                {
-                    fs.Seek(position + 4, SeekOrigin.Begin);
-                    int version1 = fs.ReadByte();
-                    int version2 = fs.ReadByte();
-                    int version = (version2 << 8) | version1;
-                    return $"{version}";
-                }
-            }
-            catch
-            {
-                return null;
-            }
+            int version1 = fileContent[position + 4];
+            int version2 = fileContent[position + 5];
+            int version = (version2 << 8) | version1;
+            return $"{version}";
         }
     }
 }
