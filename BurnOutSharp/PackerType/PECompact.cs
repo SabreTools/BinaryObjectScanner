cf77e923ee81bd0f829fda7c9b50c7328fcb0557
SilasLaspada <silasqwerty@ymail.com>
2021-03-20 22:23:13 -0600

    Address comments

--- BurnOutSharp/PackerType/PECompact.cs
+++ BurnOutSharp/PackerType/PECompact.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace BurnOutSharp.PackerType
 {
@@ -11,35 +12,24 @@ namespace BurnOutSharp.PackerType
             // Another possible version string for version 1 is "PECO" (50 45 43 4F)
 
             // "pec1"
-            byte[] check = new byte[] { 0x70, 0x65, 0x63, 0x31 };
+            byte?[] check = new byte?[] { 0x70, 0x65, 0x63, 0x31 };
             if (fileContent.FirstPosition(check, out int position, end: 2048))
                 return "PE Compact 1" + (includePosition ? $" (Index {position})" : string.Empty);
 
             // "PEC2"
-            check = new byte[] { 0x50, 0x45, 0x43, 0x32 };
+            check = new byte?[] { 0x50, 0x45, 0x43, 0x32 };
             if (fileContent.FirstPosition(check, out position, end: 2048))
             {
-                string version = GetVersion(fileContent, position);
-                if (version == null)
-                    return "PE Compact 2" + (includePosition ? $" (Index {position})" : string.Empty);
-
+                int version = BitConverter.ToInt16(fileContent, position + 4);
                 return $"PE Compact 2 v{version}" + (includePosition ? $" (Index {position})" : string.Empty);
             }
 
             // "PECompact2"
-            check = new byte[] { 0x50, 0x45, 0x43, 0x6F, 0x6D, 0x70, 0x61, 0x63, 0x74, 0x32 };
+            check = new byte?[] { 0x50, 0x45, 0x43, 0x6F, 0x6D, 0x70, 0x61, 0x63, 0x74, 0x32 };
             if (fileContent.FirstPosition(check, out position))
                 return "PE Compact 2" + (includePosition ? $" (Index {position})" : string.Empty);
 
             return null;
-        }
-
-        private static string GetVersion(byte[] fileContent, int position)
-        {
-            int version1 = fileContent[position + 4];
-            int version2 = fileContent[position + 5];
-            int version = (version2 << 8) | version1;
-            return $"{version}";
         }
     }
 }
