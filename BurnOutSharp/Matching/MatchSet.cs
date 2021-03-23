f25800510b63e2ca3dd6919ce94fc4415362fa16
Matt Nadareski <mnadareski@outlook.com>
2021-03-22 21:30:30 -0700

    MatchSet is abstract

--- BurnOutSharp/Matching/MatchSet.cs
+++ BurnOutSharp/Matching/MatchSet.cs
@@ -5,7 +5,7 @@ namespace BurnOutSharp.Matching
     /// <summary>
     /// Wrapper for a single set of matching criteria
     /// </summary>
-    internal class MatchSet<T, U> where T : IMatch<U>
+    internal abstract class MatchSet<T, U> where T : IMatch<U>
     {
         /// <summary>
         /// Set of all matchers
