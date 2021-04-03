a623a16d7676d889a93df9fe44c11e20984bb0e8
SilasLaspada <silasqwerty@ymail.com>
2021-04-03 16:38:35 -0600

    Improve Steam detection

--- BurnOutSharp/ProtectionType/Steam.cs
+++ BurnOutSharp/ProtectionType/Steam.cs
@@ -15,6 +15,15 @@ namespace BurnOutSharp.ProtectionType
                 new PathMatchSet(new PathMatch("SteamInstall.msi", useEndsWith: true), "Steam"),
                 new PathMatchSet(new PathMatch("SteamRetailInstaller.dmg", useEndsWith: true), "Steam"),
                 new PathMatchSet(new PathMatch("SteamSetup.exe", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamService.exe", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Czech.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_English.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_French.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_German.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Italian.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Polish.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Russian.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Spanish.msi", useEndsWith: true), "Steam"),
             };
 
             return MatchUtil.GetAllMatches(files, matchers, any: true);
@@ -30,6 +39,15 @@ namespace BurnOutSharp.ProtectionType
                 new PathMatchSet(new PathMatch("SteamInstall.msi", useEndsWith: true), "Steam"),
                 new PathMatchSet(new PathMatch("SteamRetailInstaller.dmg", useEndsWith: true), "Steam"),
                 new PathMatchSet(new PathMatch("SteamSetup.exe", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamService.exe", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Czech.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_English.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_French.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_German.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Italian.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Polish.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Russian.msi", useEndsWith: true), "Steam"),
+                new PathMatchSet(new PathMatch("SteamInstall_Spanish.msi", useEndsWith: true), "Steam"),
             };
 
             return MatchUtil.GetFirstMatch(path, matchers, any: true);
