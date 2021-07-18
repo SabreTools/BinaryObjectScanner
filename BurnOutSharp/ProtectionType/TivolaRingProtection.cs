2ae860e8cab827b6cbbb5d9a21f6da73f23b228c
Matt Nadareski <mnadareski@outlook.com>
2021-07-17 21:17:45 -0700

    Add Tivola Ring Protection detection (fixes #45)

---
+++ BurnOutSharp/ProtectionType/TivolaRingProtection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BurnOutSharp.Matching;
+
+namespace BurnOutSharp.ProtectionType
+{
+    /// <remarks>
+    /// This protection needs far more research
+    /// </remarks>
+    public class TivolaRingProtection : IPathCheck
+    {
+        /// <inheritdoc/>
+        public List<string> CheckDirectoryPath(string path, IEnumerable<string> files)
+        {
+            var matchers = new List<PathMatchSet>
+            {
+                new PathMatchSet(Path.Combine("ZDAT", "webmast.dxx"), "Tivola Ring Protection"),
+            };
+
+            return MatchUtil.GetAllMatches(files, matchers, any: true);
+        }
+
+        /// <inheritdoc/>
+        public string CheckFilePath(string path)
+        {
+            var matchers = new List<PathMatchSet>
+            {
+                new PathMatchSet(Path.Combine("ZDAT", "webmast.dxx"), "Tivola Ring Protection"),
+            };
+
+            return MatchUtil.GetFirstMatch(path, matchers, any: true);
+        }
+    }
+}
