d34adbc8e25c7112668aa1b0fdb849d21c28a73e
SilasLaspada <silasqwerty@ymail.com>
2021-08-23 23:46:10 -0600

    Address reviews

--- BurnOutSharp/PackerType/IntelInstallationFramework.cs
+++ BurnOutSharp/PackerType/IntelInstallationFramework.cs
@@ -6,28 +6,25 @@ namespace BurnOutSharp.PackerType
     public class IntelInstallationFramework : IContentCheck
     {
         /// <inheritdoc/>
-        public bool ShouldScan(byte[] magic) => true;
-
-        /// <inheritdoc/>
         public string CheckContents(string file, byte[] fileContent, bool includePosition = false)
         {
-            var fileDetails = Utilities.GetFileVersionInfo(file);
+            var fvinfo = Utilities.GetFileVersionInfo(file);
 
-            string name = fileDetails?.FileDescription.Trim();
-            if (!string.IsNullOrWhiteSpace(name) && name.Equals("Intel(R) Installation Framework", StringComparison.OrdinalIgnoreCase))
+            string name = fvinfo?.FileDescription.Trim();
+            if (!string.IsNullOrWhiteSpace(name)
+                && (name.Equals("Intel(R) Installation Framework", StringComparison.OrdinalIgnoreCase)
+                || name.Equals("Intel Installation Framework", StringComparison.OrdinalIgnoreCase)))
+            {
                 return $"Intel Installation Framework {Utilities.GetFileVersion(file)}";
+            }
 
-            name = fileDetails?.ProductName.Trim();
-            if (!string.IsNullOrWhiteSpace(name) && name.Equals("Intel(R) Installation Framework", StringComparison.OrdinalIgnoreCase))
+            name = fvinfo?.ProductName.Trim();
+            if (!string.IsNullOrWhiteSpace(name)
+                && (name.Equals("Intel(R) Installation Framework", StringComparison.OrdinalIgnoreCase)
+                || name.Equals("Intel Installation Framework", StringComparison.OrdinalIgnoreCase)))
+            {
                 return $"Intel Installation Framework {Utilities.GetFileVersion(file)}";
-
-            name = fileDetails?.ProductName.Trim();
-            if (!string.IsNullOrWhiteSpace(name) && name.Equals("Intel Installation Framework", StringComparison.OrdinalIgnoreCase))
-                return $"Intel Installation Framework {Utilities.GetFileVersion(file)}";
-
-            name = fileDetails?.ProductName.Trim();
-            if (!string.IsNullOrWhiteSpace(name) && name.Equals("Intel Installation Framework", StringComparison.OrdinalIgnoreCase))
-                return $"Intel Installation Framework {Utilities.GetFileVersion(file)}";
+            }
 
             return null;
         }
