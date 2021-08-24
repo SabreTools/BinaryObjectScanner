c7a62b80d1213453136351234b77ceca000003b0
SilasLaspada <silasqwerty@ymail.com>
2021-08-23 22:35:45 -0600

    Address reviews

--- BurnOutSharp/PackerType/MicrosoftCABSFX.cs
+++ BurnOutSharp/PackerType/MicrosoftCABSFX.cs
@@ -14,14 +14,13 @@ namespace BurnOutSharp.PackerType
         /// <inheritdoc/>
         public string CheckContents(string file, byte[] fileContent, bool includePosition = false)
         {
+            var fvinfo = Utilities.GetFileVersionInfo(file);
 
-            var fileDetails = Utilities.GetFileVersionInfo(file);
-
-            string name = fileDetails?.InternalName.Trim();
+            string name = fvinfo?.InternalName.Trim();
             if (!string.IsNullOrWhiteSpace(name) && name.Equals("Wextract", StringComparison.OrdinalIgnoreCase))
                 return $"Microsoft CAB SFX v{Utilities.GetFileVersion(file)}";
 
-            name = fileDetails?.OriginalFilename.Trim();
+            name = fvinfo?.OriginalFilename.Trim();
             if (!string.IsNullOrWhiteSpace(name) && name.Equals("WEXTRACT.EXE", StringComparison.OrdinalIgnoreCase))
                 return $"Microsoft CAB SFX v{Utilities.GetFileVersion(file)}";
 
@@ -34,22 +33,18 @@ namespace BurnOutSharp.PackerType
                     0x5F, 0x63, 0x6C, 0x65, 0x61, 0x6E, 0x75, 0x70
                 }, GetVersion, "Microsoft CAB SFX"),
 
+                /* This detects a different but similar type of SFX that uses Microsoft CAB files.
+                   Further research is needed to see if it's just a different version or entirely separate. */
                 // MSCFu
-                new ContentMatchSet(new byte?[] { 0x4D, 0x53, 0x43, 0x46, 0x75 }, GetVersion, "Microsoft CAB SFX (Generic?)"),
+                new ContentMatchSet(new byte?[] { 0x4D, 0x53, 0x43, 0x46, 0x75 }, GetVersion, "Microsoft CAB SFX"),
             };
 
             return MatchUtil.GetFirstMatch(file, fileContent, matchers, includePosition);
         }
 
+        // This method of version detection is suboptimal because the version is sometimes the version of the included software, not the SFX itself.
         public static string GetVersion(string file, byte[] fileContent, List<int> positions)
         {
-            // This method of version detection is suboptimal because the version is sometimes the version of the included software, not the SFX itself.
-            FileVersionInfo fileDetails = FileVersionInfo.GetVersionInfo(file);
-            return "v" + fileDetails.FileVersion;
-        }
-        public static string GetVersion(string file, byte[] fileContent)
-        {
-            // This method of version detection is suboptimal because the version is sometimes the version of the included software, not the SFX itself.
             FileVersionInfo fileDetails = FileVersionInfo.GetVersionInfo(file);
             return "v" + fileDetails.FileVersion;
         }
