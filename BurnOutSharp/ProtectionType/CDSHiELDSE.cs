9fe6b101bd515a9134646a94fda7dfd4d0ab8c01
Matt Nadareski <mnadareski@outlook.com>
2021-09-15 11:01:51 -0700

    Convert CDSHiELD SE to section-based

--- BurnOutSharp/ProtectionType/CDSHiELDSE.cs
+++ BurnOutSharp/ProtectionType/CDSHiELDSE.cs
@@ -9,14 +9,27 @@ namespace BurnOutSharp.ProtectionType
         /// <inheritdoc/>
         public string CheckContents(string file, byte[] fileContent, bool includeDebug, PortableExecutable pex, NewExecutable nex)
         {
-            // TODO: Obtain a sample to find where this string is in a typical executable
-            var contentMatchSets = new List<ContentMatchSet>
+            // Get the sections from the executable, if possible
+            var sections = pex?.SectionTable;
+            if (sections == null)
+                return null;
+
+            // Get the code/CODE section, if it exists
+            var codeSectionRaw = pex.ReadRawSection(fileContent, "code", first: true) ?? pex.ReadRawSection(fileContent, "CODE", first: true);
+            if (codeSectionRaw != null)
             {
-                // ~0017.tmp
-                new ContentMatchSet(new byte?[] { 0x7E, 0x30, 0x30, 0x31, 0x37, 0x2E, 0x74, 0x6D, 0x70 }, "CDSHiELD SE"),
-            };
-            
-            return MatchUtil.GetFirstMatch(file, fileContent, contentMatchSets, includeDebug);
+                var matchers = new List<ContentMatchSet>
+                {
+                    // ~0017.tmp
+                    new ContentMatchSet(new byte?[] { 0x7E, 0x30, 0x30, 0x31, 0x37, 0x2E, 0x74, 0x6D, 0x70 }, "CDSHiELD SE"),
+                };
+
+                string match = MatchUtil.GetFirstMatch(file, codeSectionRaw, matchers, includeDebug);
+                if (!string.IsNullOrWhiteSpace(match))
+                    return match;
+            }
+
+            return null;
         }
     }
 }
