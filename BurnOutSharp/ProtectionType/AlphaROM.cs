d2fad1ab298a06db4a4739d982e21fef632febbb
Matt Nadareski <mnadareski@outlook.com>
2021-10-29 15:19:50 -0700

    Fix Alpha-ROM... again (fixes #69)

--- BurnOutSharp/ProtectionType/AlphaROM.cs
+++ BurnOutSharp/ProtectionType/AlphaROM.cs
@@ -26,8 +26,11 @@ namespace BurnOutSharp.ProtectionType
             {
                 var matchers = new List<ContentMatchSet>
                 {
-                    // SETTEC
-                    new ContentMatchSet(new byte?[] { 0x53, 0x45, 0x54, 0x54, 0x45, 0x43 }, "Alpha-ROM"),
+                    // \SETTEC
+                    new ContentMatchSet(new byte?[] { 0x5C, 0x53, 0x45, 0x54, 0x54, 0x45, 0x43 }, "Alpha-ROM"),
+
+                    // SETTEC0000
+                    new ContentMatchSet(new byte?[] { 0x53, 0x45, 0x54, 0x54, 0x45, 0x43, 0x30, 0x30, 0x30, 0x30 }, "Alpha-ROM"),
                 };
 
                 string match = MatchUtil.GetFirstMatch(file, pex.DataSectionRaw, matchers, includeDebug);
