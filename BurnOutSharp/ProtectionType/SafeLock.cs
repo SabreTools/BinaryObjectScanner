cf87279dfcbc3f5f224c8b2a4b6fc99c99206409
Matt Nadareski <mnadareski@outlook.com>
2021-11-24 21:59:54 -0800

    Add content notes to SafeLock

--- BurnOutSharp/ProtectionType/SafeLock.cs
+++ BurnOutSharp/ProtectionType/SafeLock.cs
@@ -5,6 +5,17 @@ using BurnOutSharp.Matching;
 namespace BurnOutSharp.ProtectionType
 {
     // https://www.cdrinfo.pl/cdr/porady/safelock/safelock.php3
+    // Notes for possible future content checks:
+    //  - Protected files (that haven't been renamed) will contain a couple of strings at the end:
+    //      + SafeLock.dat
+    //      + Proszê w³o¿yæ p³ytê CD do CDROM-u
+    //      + Proszê w³o¿yæ oryginaln¹ p³ytê CD do CDROM-u
+    //  - Also at the end is an ASCII number possibly indicating version:
+    //      + 0.99.15 uses "3144288522"
+    //      + 0.99.22 uses "1614884465"
+    //      + 1.0.4   uses "3574069264"
+    //  - All of the above is at the very end of the file
+    //  - All auxiliary files (like .001, .128, and .dat) seem to be encrypted or padding data
     public class SafeLock : IPathCheck
     {
         /// <inheritdoc/>
