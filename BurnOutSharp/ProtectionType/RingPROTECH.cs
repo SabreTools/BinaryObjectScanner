b581cb312402fbe4b9ec27aa26e6590def69a88f
Matt Nadareski <mnadareski@outlook.com>
2022-02-04 15:24:05 -0800

    Disable content checks for RPT/ProRing

--- BurnOutSharp/ProtectionType/RingPROTECH.cs
+++ BurnOutSharp/ProtectionType/RingPROTECH.cs
@@ -12,17 +12,18 @@ namespace BurnOutSharp.ProtectionType
         public string CheckContents(string file, byte[] fileContent, bool includeDebug, PortableExecutable pex, NewExecutable nex)
         {
             // TODO: Obtain a sample to find where this string is in a typical executable
-            var contentMatchSets = new List<ContentMatchSet>
-            {
-                // (char)0x00 + Allocator + (char)0x00 + (char)0x00 + (char)0x00 + (char)0x00
-                new ContentMatchSet(new byte?[]
-                {
-                    0x00, 0x41, 0x6C, 0x6C, 0x6F, 0x63, 0x61, 0x74,
-                    0x6F, 0x72, 0x00, 0x00, 0x00, 0x00
-                }, "Ring PROTECH / ProRing [Check disc for physical ring]"),
-            };
+            // var contentMatchSets = new List<ContentMatchSet>
+            // {
+            //     // (char)0x00 + Allocator + (char)0x00 + (char)0x00 + (char)0x00 + (char)0x00
+            //     new ContentMatchSet(new byte?[]
+            //     {
+            //         0x00, 0x41, 0x6C, 0x6C, 0x6F, 0x63, 0x61, 0x74,
+            //         0x6F, 0x72, 0x00, 0x00, 0x00, 0x00
+            //     }, "Ring PROTECH / ProRing [Check disc for physical ring]"),
+            // };
             
-            return MatchUtil.GetFirstMatch(file, fileContent, contentMatchSets, includeDebug);
+            // return MatchUtil.GetFirstMatch(file, fileContent, contentMatchSets, includeDebug);
+            return null;
         }
 
         // TODO: Confirm if these checks are only for ProRing or if they are also for older Ring PROTECH
