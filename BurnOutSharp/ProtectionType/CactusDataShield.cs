e80034abf1fb00d6da1600f2142e436ccb5ff3f0
Matt Nadareski <mnadareski@outlook.com>
2022-03-14 11:52:49 -0700

    Simplify CDS code a bit

--- BurnOutSharp/ProtectionType/CactusDataShield.cs
+++ BurnOutSharp/ProtectionType/CactusDataShield.cs
@@ -13,27 +13,21 @@ namespace BurnOutSharp.ProtectionType
     public class CactusDataShield : IContentCheck, IPEContentCheck, IPathCheck
     {
         /// <inheritdoc/>
-        private List<ContentMatchSet> GetContentMatchSets()
-        {
-            // TODO: Both of these are found in Mac binaries
-            return new List<ContentMatchSet>
-            {
-                // CDSPlayer
-                new ContentMatchSet(new byte?[] { 0x43, 0x44, 0x53, 0x50, 0x6C, 0x61, 0x79, 0x65, 0x72 }, "Cactus Data Shield 200"),
-
-                // yucca.cds
-                new ContentMatchSet(new byte?[] { 0x79, 0x75, 0x63, 0x63, 0x61, 0x2E, 0x63, 0x64, 0x73 }, "Cactus Data Shield 200"),
-            };
-        }
-
-        /// <inheritdoc/>
         public string CheckContents(string file, byte[] fileContent, bool includeDebug, PortableExecutable pex, NewExecutable nex)
         {
             // TODO: Limit these checks to Mac binaries
             // TODO: Obtain a sample to find where this string is in a typical executable
             if (includeDebug)
             {
-                var contentMatchSets = GetContentMatchSets();
+                var contentMatchSets = new List<ContentMatchSet>
+                {
+                    // CDSPlayer
+                    new ContentMatchSet(new byte?[] { 0x43, 0x44, 0x53, 0x50, 0x6C, 0x61, 0x79, 0x65, 0x72 }, "Cactus Data Shield 200"),
+
+                    // yucca.cds
+                    new ContentMatchSet(new byte?[] { 0x79, 0x75, 0x63, 0x63, 0x61, 0x2E, 0x63, 0x64, 0x73 }, "Cactus Data Shield 200"),
+                };
+
                 if (contentMatchSets != null && contentMatchSets.Any())
                     return MatchUtil.GetFirstMatch(file, fileContent, contentMatchSets, includeDebug);
             }
