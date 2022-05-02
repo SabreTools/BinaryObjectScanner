6f9e92d2226f4a5fdab126e8befd56bd355143a8
Matt Nadareski <mnadareski@outlook.com>
2022-05-01 21:03:48 -0700

    Clean up interface comments

--- BurnOutSharp/Interfaces/IPathCheck.cs
+++ BurnOutSharp/Interfaces/IPathCheck.cs
@@ -10,14 +10,14 @@ namespace BurnOutSharp.Interfaces
         /// </summary>
         /// <param name="path">Path to check for protection indicators</param>
         /// <param name="files">Enumerable of strings representing files in a directory</param>
-        /// <remarks>This can do some limited content checking as well, but it's suggested to use IContentCheck instead, if possible</remarks>
+        /// <remarks>This can do some limited content checking as well, but it's suggested to use a content check instead, if possible</remarks>
         ConcurrentQueue<string> CheckDirectoryPath(string path, IEnumerable<string> files);
 
         /// <summary>
         /// Check a file path for protections based on path name
         /// </summary>
         /// <param name="path">Path to check for protection indicators</param>
-        /// <remarks>This can do some limited content checking as well, but it's suggested to use IContentCheck instead, if possible</remarks>
+        /// <remarks>This can do some limited content checking as well, but it's suggested to use a content check instead, if possible</remarks>
         string CheckFilePath(string path);
     }
 }
