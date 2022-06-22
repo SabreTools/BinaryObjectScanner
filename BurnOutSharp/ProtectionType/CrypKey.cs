2209f362facdceace57490de1e8196c61819be78
Matt Nadareski <mnadareski@outlook.com>
2022-06-22 09:50:16 -0700

    Add notes for CrypKey

--- BurnOutSharp/ProtectionType/CrypKey.cs
+++ BurnOutSharp/ProtectionType/CrypKey.cs
@@ -3,5 +3,8 @@
     public class CrypKey
     {
         // TODO: Implement - http://www.crypkey.com/products/cdlock/cdmain.html
+
+        // https://github.com/horsicq/Detect-It-Easy/blob/master/db/PE/CrypKey%20Installer.1.sg
+        // https://github.com/horsicq/Detect-It-Easy/blob/master/db/PE/CrypKey.2.sg
     }
 }
