15de6b242bfa1508245d7e129aa1768bcfdbdaf5
SilasLaspada <silasqwerty@ymail.com>
2022-07-04 19:57:00 -0600

    Add shell for Themida
    Add shell and a few notes for Themida/WinLicense/Code Virtualize.

---
+++ BurnOutSharp/ProtectionType/Themida.cs
@@ -0,0 +1,12 @@
+﻿namespace BurnOutSharp.ProtectionType
+{
+    /// <summary>
+    /// Protection created by Oreans and in use since at least before 2009. Known to be used in Book/Music Collector (http://www.alwinhoogerdijk.com/2009/12/24/protecting-software-with-themida/).
+    /// May possibly be used in the PC release of Dynasty Warriors 7 with Xtreme Legends (https://www.pcgamingwiki.com/wiki/Dynasty_Warriors_7_with_Xtreme_Legends).
+    /// Themida/WinLicense/Code Virtualize seem to all be interconnected/related to each other, or are all at least part of the SecureEngine suite together (https://www.oreans.com/products.php).
+    /// </summary>
+    public class Themida
+    {
+        // TODO: Implement - https://www.oreans.com/
+    }
+}
