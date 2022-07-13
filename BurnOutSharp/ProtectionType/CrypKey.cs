313cc2bfb8ed14b069a53adfe2025e7a33d15561
Matt Nadareski <mnadareski@outlook.com>
2022-07-13 12:18:39 -0700

    Add another source for CrypKey

--- BurnOutSharp/ProtectionType/CrypKey.cs
+++ BurnOutSharp/ProtectionType/CrypKey.cs
@@ -6,5 +6,6 @@
 
         // https://github.com/horsicq/Detect-It-Easy/blob/master/db/PE/CrypKey%20Installer.1.sg
         // https://github.com/horsicq/Detect-It-Easy/blob/master/db/PE/CrypKey.2.sg
+        // https://github.com/wolfram77web/app-peid/blob/master/userdb.txt
     }
 }
