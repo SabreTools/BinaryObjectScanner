42da9f4a821507537a66fa3041cd890ef7cbe6b6
SilasLaspada <silasqwerty@ymail.com>
2022-07-12 23:57:22 -0600

    Add support for detecting Cucko (#132)
    * Add support for detecting Cucko
    
    Add support for detecting Cucko.
    
    * Remove debug line

--- BurnOutSharp/ProtectionType/ElectronicArts.cs
+++ BurnOutSharp/ProtectionType/ElectronicArts.cs
@@ -13,6 +13,7 @@ namespace BurnOutSharp.ProtectionType
     //      - Look into `ccinstall`, `Services/EACOM`, `TSLHost`, `SIGS/UploadThread/exchangeAuthToken`,
     //          `blazeURL`, `psapi.dll`, `DasmX86Dll.dll`, `NVCPL.dll`, `iphlpapi.dll`, `dbghelp.dll`,
     //          `WS2_32.dll`, 
+    //      - Cucko is confirmed to, at least, use DMI checks.
     public class ElectronicArts : IPortableExecutableCheck
     {
         /// <inheritdoc/>
@@ -88,6 +89,15 @@ namespace BurnOutSharp.ProtectionType
                         0x41, 0x00, 0x00, 0x00, 0x41, 0x63, 0x74, 0x69,
                         0x76, 0x61, 0x74, 0x69, 0x6F, 0x6E
                     }, "EA DRM Protection"),
+
+                    // Confirmed to detect most examples known of Cucko. The only known exception is the version of "TSLHost.dll" included on Redump entry 36119.
+                    // ŠU‰8...…™...ŠUŠ8T...
+                    new ContentMatchSet(new byte?[]
+                    {
+                        0x8A, 0x55, 0x89, 0x38, 0x14, 0x1E, 0x0F, 0x85, 
+                        0x99, 0x00, 0x00, 0x00, 0x8A, 0x55, 0x8A, 0x38, 
+                        0x54, 0x1E, 0x01, 0x0F
+                    }, "Cucko (Custom EA Protection)")
                 };
 
                 string match = MatchUtil.GetFirstMatch(file, pex.TextSectionRaw, matchers, includeDebug);
