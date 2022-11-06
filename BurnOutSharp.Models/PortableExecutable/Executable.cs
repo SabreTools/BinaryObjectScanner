c5d005bdeb8c696adfe85fef3f5fd0a1e54831ca
Matt Nadareski <mnadareski@outlook.com>
2022-11-05 21:37:54 -0700

    Add PE export address table entries

--- BurnOutSharp.Models/PortableExecutable/Executable.cs
+++ BurnOutSharp.Models/PortableExecutable/Executable.cs
@@ -56,6 +56,6 @@ namespace BurnOutSharp.Models.PortableExecutable
         /// </summary>
         public DelayLoadDirectoryTableEntry[] DelayLoadDirectoryTable { get; set; }
 
-        // TODO: Left off at "Export Address Table"
+        // TODO: Left off at "Import Directory Table"
     }
 }
