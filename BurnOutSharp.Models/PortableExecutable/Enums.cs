f7343ea3053d05780ec1d0556b5d5401730a5696
Matt Nadareski <mnadareski@outlook.com>
2022-11-09 11:35:31 -0800

    Update PE resource type enum

--- BurnOutSharp.Models/PortableExecutable/Enums.cs
+++ BurnOutSharp.Models/PortableExecutable/Enums.cs
@@ -1738,20 +1738,114 @@ namespace BurnOutSharp.Models.PortableExecutable
         RT_NEWRESOURCE = 0x2000,
         RT_ERROR = 0x7FFF,
 
+        /// <summary>
+        /// Hardware-dependent cursor resource.
+        /// </summary>
         RT_CURSOR = 1,
+
+        /// <summary>
+        /// Bitmap resource.
+        /// </summary>
         RT_BITMAP = 2,
+
+        /// <summary>
+        /// Hardware-dependent icon resource.
+        /// </summary>
         RT_ICON = 3,
+
+        /// <summary>
+        /// Menu resource.
+        /// </summary>
         RT_MENU = 4,
+
+        /// <summary>
+        /// Dialog box.
+        /// </summary>
         RT_DIALOG = 5,
+
+        /// <summary>
+        /// String-table entry.
+        /// </summary>
         RT_STRING = 6,
+
+        /// <summary>
+        /// Font directory resource.
+        /// </summary>
         RT_FONTDIR = 7,
+
+        /// <summary>
+        /// Font resource.
+        /// </summary>
         RT_FONT = 8,
-        RT_ACCELERATORS = 9,
+
+        /// <summary>
+        /// Accelerator table.
+        /// </summary>
+        RT_ACCELERATOR = 9,
+
+        /// <summary>
+        /// Application-defined resource (raw data).
+        /// </summary>
         RT_RCDATA = 10,
+
+        /// <summary>
+        /// Message-table entry.
+        /// </summary>
         RT_MESSAGETABLE = 11,
-        RT_GROUP_CURSOR = 12,
-        RT_GROUP_ICON = 14,
+
+        /// <summary>
+        /// Hardware-independent cursor resource.
+        /// </summary>
+        RT_GROUP_CURSOR = RT_CURSOR + 11,
+
+        /// <summary>
+        /// Hardware-independent icon resource.
+        /// </summary>
+        RT_GROUP_ICON = RT_ICON + 11,
+
+        /// <summary>
+        /// Version resource.
+        /// </summary>
         RT_VERSION = 16,
+
+        /// <summary>
+        /// Allows a resource editing tool to associate a string with an .rc file.
+        /// Typically, the string is the name of the header file that provides symbolic
+        /// names. The resource compiler parses the string but otherwise ignores the
+        /// value. For example, `1 DLGINCLUDE "MyFile.h"`
+        /// </summary>
+        RT_DLGINCLUDE = 17,
+
+        /// <summary>
+        /// Plug and Play resource.
+        /// </summary>
+        RT_PLUGPLAY = 19,
+
+        /// <summary>
+        /// VXD.
+        /// </summary>
+        RT_VXD = 20,
+
+        /// <summary>
+        /// Animated cursor.
+        /// </summary>
+        RT_ANICURSOR = 21,
+
+        /// <summary>
+        /// Animated icon.
+        /// </summary>
+        RT_ANIICON = 22,
+
+        /// <summary>
+        /// HTML resource.
+        /// </summary>
+        RT_HTML = 23,
+
+        /// <summary>
+        /// Side-by-Side Assembly Manifest.
+        /// </summary>
+        RT_MANIFEST = 24,
+
         RT_NEWBITMAP = (RT_BITMAP | RT_NEWRESOURCE),
         RT_NEWMENU = (RT_MENU | RT_NEWRESOURCE),
         RT_NEWDIALOG = (RT_DIALOG | RT_NEWRESOURCE),
