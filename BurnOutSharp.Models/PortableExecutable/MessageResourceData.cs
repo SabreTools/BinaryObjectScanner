27d53abd101d403e09b4a45b7fc608745edaaafc
Matt Nadareski <mnadareski@outlook.com>
2022-11-11 15:41:37 -0800

    Add PE message resource models

---
+++ BurnOutSharp.Models/PortableExecutable/MessageResourceData.cs
@@ -0,0 +1,25 @@
+﻿namespace BurnOutSharp.Models.PortableExecutable
+{
+    /// <summary>
+    /// Contains information about formatted text for display as an error message or in a message
+    /// box in a message table resource.
+    /// </summary>
+    /// <see href="https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-message_resource_data"/>
+    public class MessageResourceData
+    {
+        /// <summary>
+        /// The number of MESSAGE_RESOURCE_BLOCK structures.
+        /// </summary>
+        public uint NumberOfBlocks;
+
+        /// <summary>
+        /// An array of structures. The array is the size indicated by the NumberOfBlocks member.
+        /// </summary>
+        public MessageResourceBlock[] Blocks;
+
+        /// <summary>
+        /// Message resource entries
+        /// </summary>
+        public MessageResourceEntry[] Entries;
+    }
+}
