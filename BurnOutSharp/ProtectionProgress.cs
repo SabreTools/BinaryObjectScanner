912b49ecc482e074877a7530d061caf0f9692bce
Matt Nadareski <mnadareski@outlook.com>
2022-12-08 22:37:57 -0800

    Cleanup `ProtectionProgress`

--- BurnOutSharp/ProtectionProgress.cs
+++ BurnOutSharp/ProtectionProgress.cs
@@ -1,9 +1,23 @@
 ﻿namespace BurnOutSharp
 {
-    public class ProtectionProgress
+    /// <summary>
+    /// Struct representing protection scanning progress
+    /// </summary>
+    public struct ProtectionProgress
     {
+        /// <summary>
+        /// Filename to report progress for
+        /// </summary>
         public string Filename { get; private set; }
+
+        /// <summary>
+        /// Value between 0 and 1 representign the percentage completed
+        /// </summary>
         public float Percentage { get; private set; }
+
+        /// <summary>
+        /// Protection information to report
+        /// </summary>
         public string Protection { get; private set; }
 
         public ProtectionProgress(string filename, float percentage, string protection)
