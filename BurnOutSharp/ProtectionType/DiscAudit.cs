d11782a87b0d8d7932a9d81ad0c62aedd029b12b
SilasLaspada <silasqwerty@ymail.com>
2022-12-12 00:13:22 -0700

    Add notes about various TTR protections
    * Add notes about DiscGuard's use of subchannels.
    
    * Add reference to DiscAudit and MusicGuard.

--- BurnOutSharp/ProtectionType/DiscAudit.cs
+++ BurnOutSharp/ProtectionType/DiscAudit.cs
@@ -3,6 +3,7 @@
     public class DiscAudit
     {
         // TODO: Implement - https://web.archive.org/web/19981206095259/http://www.ttrtech.com/discaudi.htm
+        // DiscAudit was active in at least 1998 (https://web.archive.org/web/19991013001235/http://ttrtech.com/pressrel.htm).
         // References and further information:
         // https://www.sec.gov/Archives/edgar/data/933955/0000950117-97-001817.txt
         // https://books.google.com/books?id=3gkEAAAAMBAJ&pg=PA39
