fa3ccf9953dd6adad4d6dcac5431579822002b68
Matt Nadareski <mnadareski@outlook.com>
2022-12-12 21:25:00 -0800

    Add more SecuROM AddD section notes

--- BurnOutSharp.Models/PortableExecutable/SecuROMAddD.cs
+++ BurnOutSharp.Models/PortableExecutable/SecuROMAddD.cs
@@ -21,6 +21,10 @@ namespace BurnOutSharp.Models.PortableExecutable
         /// <summary>
         /// Unknown (Entry count?)
         /// </summary>
+        /// <remarks>
+        /// 3 in EXPUNGED, 3.17.00.0017, 3.17.00.0019
+        /// 3 in 4.47.00.0039, 4.84.00.0054, 4.84.69.0037, 4.84.76.7966, 4.84.76.7968, 4.85.07.0009
+        /// </remarks>
         public uint EntryCount;
 
         /// <summary>
@@ -34,70 +38,19 @@ namespace BurnOutSharp.Models.PortableExecutable
         public char[] Build;
 
         /// <summary>
-        /// Unknown (0x14)
+        /// Unknown (0x14h), Variable number of bytes before entry table
         /// </summary>
-        /// <remarks>498211340 [6668, 3762] in the sample</remarks>
-        public uint Unknown14h;
-
-        /// <summary>
-        /// Unknown (0x18), possibly entry 1 date/time?
-        /// </summary>
-        /// <remarks>783556112 [7696, 11956] / 1994-10-30 22:28:32 in the sample</remarks>
-        public uint Unknown18h;
-
-        /// <summary>
-        /// Unknown (0x1C), possibly entry 2 date/time?
-        /// </summary>
-        /// <remarks>861548360 [12104, 13146] / 1997-04-20 14:59:20 in the sample</remarks>
-        public uint Unknown1Ch;
-
-        /// <summary>
-        /// Unknown (0x20), possibly entry 3 date/time?
-        /// </summary>
-        /// <remarks>879113110 [13206, 13414] / 1997-11-09 22:05:10 in the sample</remarks>
-        public uint Unknown20h;
-
-        /// <summary>
-        /// Unknown (0x24)
-        /// </summary>
-        /// <remarks>4212741 [18437, 64] in the sample</remarks>
-        public uint Unknown24h;
-
-        /// <summary>
-        /// Unknown (0x28)
-        /// </summary>
-        /// <remarks>1364015180 [14412, 20813] in the sample</remarks>
-        public uint Unknown28h;
-
-        /// <summary>
-        /// Unknown (0x2C)
-        /// </summary>
-        /// <remarks>2 [2, 0] in the sample</remarks>
-        public uint Unknown2Ch;
-
-        /// <summary>
-        /// Unknown (0x30)
-        /// </summary>
-        /// <remarks>4270144 [10304, 65] in the sample</remarks>
-        public uint Unknown30h;
-
-        /// <summary>
-        /// Unknown (0x34)
-        /// </summary>
-        /// <remarks>132 [132, 0] in the sample</remarks>
-        public uint Unknown34h;
-
-        /// <summary>
-        /// Unknown (0x38)
-        /// </summary>
-        /// <remarks>3147176 [1448, 48] in the sample</remarks>
-        public uint Unknown38h;
-
-        /// <summary>
-        /// Unknown (0x3C)
-        /// </summary>
-        /// <remarks>1244976 [65328, 18] in the sample</remarks>
-        public uint Unknown3Ch;
+        /// <remarks>
+        /// 44 bytes in EXPUNGED, 3.17.00.0017, 3.17.00.0019, 4.47.00.0039
+        /// 112 bytes in 4.84.00.0054, 4.84.69.0037, 4.84.76.7966, 4.84.76.7968, 4.85.07.0009
+        /// 112 byte range contains a fixed-length string at 0x2C, possibly a product ID?
+        /// "801400-001" in 4.84.00.0054
+        /// "594130-001" in 4.84.69.0037
+        /// "554900-001" in 4.84.76.7966
+        /// "554900-001" in 4.84.76.7968
+        /// "548520-001" in 4.85.07.0009
+        /// </remarks>
+        public byte[] Unknown14h;
 
         /// <summary>
         /// Entry table
--- BurnOutSharp.Models/PortableExecutable/SecuROMAddDEntry.cs
+++ BurnOutSharp.Models/PortableExecutable/SecuROMAddDEntry.cs
@@ -21,57 +21,57 @@ namespace BurnOutSharp.Models.PortableExecutable
         /// <summary>
         /// Length of the embedded file
         /// </summary>
-        /// <remarks>The last entry seems to be 4 bytes short</remarks>
+        /// <remarks>The last entry seems to be 4 bytes short in 4.47.00.0039</remarks>
         public uint Length;
 
         /// <summary>
         /// Unknown (0x08)
         /// </summary>
-        /// <remarks>3149224 [3496, 48] in the sample (all 3 entries)</remarks>
+        /// <remarks>3149224 [3496, 48] in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public uint Unknown08h;
 
         /// <summary>
         /// Unknown (0x0C)
         /// </summary>
-        /// <remarks>3147176 [1448, 48] in the sample (all 3 entries)</remarks>
+        /// <remarks>3147176 [1448, 48] in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public uint Unknown0Ch;
 
         /// <summary>
         /// Unknown (0x10)
         /// </summary>
-        /// <remarks>3149224 [3496, 48] in the sample (all 3 entries)</remarks>
+        /// <remarks>3149224 [3496, 48] in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public uint Unknown10h;
 
         /// <summary>
         /// Unknown (0x14)
         /// </summary>
-        /// <remarks>1245044 [65396, 18] in the sample (all 3 entries)</remarks>
+        /// <remarks>1245044 [65396, 18] in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public uint Unknown14h;
 
         /// <summary>
         /// Unknown (0x18)
         /// </summary>
-        /// <remarks>4214725 [20421, 64] in the sample (all 3 entries)</remarks>
+        /// <remarks>4214725 [20421, 64] in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public uint Unknown18h;
 
         /// <summary>
         /// Unknown (0x1C)
         /// </summary>
-        /// <remarks>2 [2, 0] in the sample (all 3 entries)</remarks>
+        /// <remarks>2 [2, 0] in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public uint Unknown1Ch;
 
         /// <summary>
         /// Entry file name (null-terminated)
         /// </summary>
-        /// <remarks>12 bytes long in the sample (all 3 entries)</remarks>
+        /// <remarks>12 bytes long in the sample (all 3 entries) in 4.47.00.0039</remarks>
         public string FileName;
 
         /// <summary>
         /// Unknown (0x2C)
         /// </summary>
         /// <remarks>
-        /// Offset based on consistent-sized filenames (12 bytes)
-        /// 132 [132, 0] in the sample (all 3 entries)
+        /// Offset based on consistent-sized filenames (12 bytes) in 4.47.00.0039
+        /// 132 [132, 0] in the sample (all 3 entries) in 4.47.00.0039
         /// </remarks>
         public uint Unknown2Ch;
     }
