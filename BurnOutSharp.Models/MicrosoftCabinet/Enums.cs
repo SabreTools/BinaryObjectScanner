318a89a4bce672e7a1306c29b9f5f45731b8f4f3
Matt Nadareski <mnadareski@outlook.com>
2022-12-16 09:48:52 -0800

    Move some LZX things to models

--- BurnOutSharp.Models/MicrosoftCabinet/Enums.cs
+++ BurnOutSharp.Models/MicrosoftCabinet/Enums.cs
@@ -138,4 +138,50 @@ namespace BurnOutSharp.Models.MicrosoftCabinet
         /// </summary>
         RESERVE_PRESENT = 0x0004,
     }
+
+    /// <summary>
+    /// 3-bit block type
+    /// </summary>
+    public enum LZXBlockType : byte
+    {
+        /// <summary>
+        /// Not valid
+        /// </summary>
+        INVALID_0 = 0b000,
+
+        /// <summary>
+        /// Verbatim block
+        /// </summary>
+        Verbatim = 0b001,
+
+        /// <summary>
+        /// Aligned offset block
+        /// </summary>
+        AlignedOffset = 0b010,
+
+        /// <summary>
+        /// Uncompressed block
+        /// </summary>
+        Uncompressed = 0b011,
+
+        /// <summary>
+        /// Not valid
+        /// </summary>
+        INVALID_4 = 0b100,
+
+        /// <summary>
+        /// Not valid
+        /// </summary>
+        INVALID_5 = 0b101,
+
+        /// <summary>
+        /// Not valid
+        /// </summary>
+        INVALID_6 = 0b110,
+
+        /// <summary>
+        /// Not valid
+        /// </summary>
+        INVALID_7 = 0b111,
+    }
 }
---
+++ BurnOutSharp.Models/MicrosoftCabinet/LZX/VerbatimBlock.cs
@@ -0,0 +1,54 @@
+namespace BurnOutSharp.Models.MicrosoftCabinet.LZX
+{
+    /// <summary>
+    /// The fields of a verbatim block that follow the generic block header
+    /// </summary>
+    /// <see href="https://interoperability.blob.core.windows.net/files/MS-PATCH/%5bMS-PATCH%5d.pdf"/>
+    public class VerbatimBlock
+    {
+        /// <summary>
+        /// Generic block header
+        /// </summary>
+        public BlockHeader Header;
+
+        /// <summary>
+        /// Pretree for first 256 elements of main tree
+        /// </summary>
+        /// <remarks>20 elements, 4 bits each</remarks>
+        public byte[] PretreeFirst256;
+
+        /// <summary>
+        /// Path lengths of first 256 elements of main tree
+        /// </summary>
+        /// <remarks>Encoded using pretree</remarks>
+        public int[] PathLengthsFirst256;
+
+        /// <summary>
+        /// Pretree for remainder of main tree
+        /// </summary>
+        /// <remarks>20 elements, 4 bits each</remarks>
+        public byte[] PretreeRemainder;
+
+        /// <summary>
+        /// Path lengths of remaining elements of main tree
+        /// </summary>
+        /// <remarks>Encoded using pretree</remarks>
+        public int[] PathLengthsRemainder;
+
+        /// <summary>
+        /// Pretree for length tree
+        /// </summary>
+        /// <remarks>20 elements, 4 bits each</remarks>
+        public byte[] PretreeLengthTree;
+
+        /// <summary>
+        /// Path lengths of elements in length tree
+        /// </summary>
+        /// <remarks>Encoded using pretree</remarks>
+        public int[] PathLengthsLengthTree;
+
+        // Entry                                    Comments                    Size
+        // ---------------------------------------------------------------------------------------
+        // Token sequence (matches and literals)    Specified in section 2.6    Variable
+    }
+}
