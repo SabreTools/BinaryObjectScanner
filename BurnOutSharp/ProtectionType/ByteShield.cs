12b48fe76f9c001a3a5af8ec49dfaf7f9bf96e59
SilasLaspada <silasqwerty@ymail.com>
2022-12-20 00:44:21 -0700

    Flash finally fucking found ByteShield
    * Update and confirm existing ByteShield checks.
    
    * Add numerous new ByteShield checks and update notes.
    
    * Update README.

--- BurnOutSharp/ProtectionType/ByteShield.cs
+++ BurnOutSharp/ProtectionType/ByteShield.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using BurnOutSharp.Interfaces;
 using BurnOutSharp.Matching;
+using BurnOutSharp.Wrappers;
 
 namespace BurnOutSharp.ProtectionType
 {
@@ -13,6 +16,7 @@ namespace BurnOutSharp.ProtectionType
     /// Patent relating to ByteShield: https://patentimages.storage.googleapis.com/ed/76/c7/d98a56aeeca2e9/US7716474.pdf and https://patents.google.com/patent/US20100212028.
     /// 
     /// Games known to use it: 
+    /// Line Rider 2: Unbound (https://fileforums.com/showthread.php?t=86909).
     /// Football Manager 2011 (https://community.sigames.com/forums/topic/189163-for-those-of-you-struggling-with-byteshield-activation-issues/).
     /// 
     /// Publishers known to use it:
@@ -35,15 +39,67 @@ namespace BurnOutSharp.ProtectionType
     /// https://www.ftc.gov/sites/default/files/documents/public_comments/ftc-town-hall-address-digital-rights-management-technologies-event-takes-place-wednesday-march-25/539814-00707.pdf
     /// https://www.gamesindustry.biz/byteshield-drm-system-now-protecting-over-200-games
     /// </summary>
-    public class ByteShield : IPathCheck
+    public class ByteShield : IPathCheck, IPortableExecutableCheck
     {
+        /// <inheritdoc/>
+        public string CheckPortableExecutable(string file, PortableExecutable pex, bool includeDebug)
+        {
+            // TODO: Add check for "ByteShield" in "LineRider2.exe" that includes version parsing.
+
+            // Get the sections from the executable, if possible
+            var sections = pex?.SectionTable;
+            if (sections == null)
+                return null;
+
+            var strings = pex.GetFirstSectionStrings(".ret");
+            if (strings != null && strings.Any())
+            {
+                // TODO: Figure out if this specifically indicates if the file is a protected exe.
+                // Found in "LineRider2.bbz" in IA item (Redump entry 6236).
+                if (strings.Any(s => s?.Contains("ByteShield") == true))
+                    return "ByteShield";
+            };
+
+            strings = pex.GetFirstSectionStrings(".rdata");
+            if (strings != null && strings.Any())
+            {
+                // Found in "ByteShield.dll" in IA item (Redump entry 6236).
+                if (strings.Any(s => s?.Contains("Byteshield0") == true))
+                    return "ByteShield";
+
+                // Found in "ByteShield.dll" in IA item (Redump entry 6236).
+                if (strings.Any(s => s?.Contains("Byte|Shield") == true))
+                    return "ByteShield";
+
+                // Found in "ByteShield.dll" in IA item (Redump entry 6236).
+                if (strings.Any(s => s?.Contains("bbz650.tmp") == true))
+                    return "ByteShield";
+            };
+
+            string name = pex.FileDescription;
+
+            // Found in "LineRider2.exe" (Redump entry 6236).
+            if (name?.StartsWith("ByteShield Client", StringComparison.OrdinalIgnoreCase) == true)
+                return $"ByteShield Activation Client";
+
+            name = pex.ProductName;
+
+            // Found in "LineRider2.exe" (Redump entry 6236).
+            if (name?.StartsWith("ByteShield Client", StringComparison.OrdinalIgnoreCase) == true)
+                return $"ByteShield Activation Client";
+
+            return null;
+        }
+
         /// <inheritdoc/>
         public ConcurrentQueue<string> CheckDirectoryPath(string path, IEnumerable<string> files)
         {
+            // TODO: Investigate reference to "bbz650.tmp" in "Byteshield.dll" (Redump entry 6236).
             var matchers = new List<PathMatchSet>
             {
-                new PathMatchSet(new PathMatch("Byteshield.dll", useEndsWith: true), "ByteShield (Unconfirmed - Please report to us on Github)"),
-                new PathMatchSet(new PathMatch(".bbz", useEndsWith: true), "ByteShield (Unconfirmed - Please report to us on Github)"),
+                // Files with the ".bbz" extension are associated with ByteShield, but the extenstion is known to be used in other places as well.
+                new PathMatchSet(new PathMatch("Byteshield.dll", useEndsWith: true), "ByteShield"),
+                new PathMatchSet(new PathMatch("Byteshield.ini", useEndsWith: true), "ByteShield"),
             };
 
             return MatchUtil.GetAllMatches(files, matchers, any: true);
@@ -54,8 +110,9 @@ namespace BurnOutSharp.ProtectionType
         {
             var matchers = new List<PathMatchSet>
             {
-                new PathMatchSet(new PathMatch("Byteshield.dll", useEndsWith: true), "ByteShield (Unconfirmed - Please report to us on Github)"),
-                new PathMatchSet(new PathMatch(".bbz", useEndsWith: true), "ByteShield (Unconfirmed - Please report to us on Github)"),
+                // Files with the ".bbz" extension are associated with ByteShield, but the extenstion is known to be used in other places as well.
+                new PathMatchSet(new PathMatch("Byteshield.dll", useEndsWith: true), "ByteShield"),
+                new PathMatchSet(new PathMatch("Byteshield.ini", useEndsWith: true), "ByteShield"),
             };
 
             return MatchUtil.GetFirstMatch(path, matchers, any: true);
