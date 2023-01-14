e78bb8cb41a6e0c27032b954ff54a3f2fadfea0f
Matt Nadareski <mnadareski@outlook.com>
2023-01-14 14:19:38 -0800

    Add PLJ header format notes

--- BurnOutSharp/FileType/PLJ.cs
+++ BurnOutSharp/FileType/PLJ.cs
@@ -37,6 +37,36 @@ namespace BurnOutSharp.FileType
                     AppendToDictionary(protections, file, "PlayJ Audio File");
                     return protections;
                 }
+
+                // Header Layout
+                // ------------------------------------------------------------------------------
+                // 0x00                                 Signature                       UInt32
+                // 0x04                                 Version?                        UInt32
+                // 0x08                                 UNKNOWN                         byte[34]
+                // 0x2A                                 Track Title Length              UInt16
+                // 0x2C                                 Track Title                     String
+                // 0x2C+TTL                             Track Artist Length             UInt16
+                // 0x2E+TTL                             Track Artist                    String
+                // 0x2E+TTL+TAL                         Album Length                    UInt16
+                // 0x30+TTL+TAL                         Album                           String
+                // 0x30+TTL+TAL+AL                      Songwriter Length               UInt16
+                // 0x32+TTL+TAL+AL                      Songwriter                      String
+                // 0x32+TTL+TAL+AL+AAL                  Copyright Owner Length          UInt16
+                // 0x34+TTL+TAL+AL+AAL                  Copyright Owner                 String
+                // 0x34+TTL+TAL+AL+AAL+COL              Distributor Length              UInt16
+                // 0x36+TTL+TAL+AL+AAL+COL              Distributor                     String
+                //
+                // The following samples also have a "Credits" section after the distributor, same format
+                // - golden_empire.plj
+                // - i_want_to_take_you_higher.plj
+                //
+                // 0x36+TTL+TAL+AL+AAL+COL+DL           Metadata? Length                UInt32
+                // 0x38+TTL+TAL+AL+AAL+COL+DL           Metadata?                       byte[DL]
+                //      Most samples hae repeating or semi-repeating values here, roughly UInt32-sized
+                // 0x38+TTL+TAL+AL+AAL+COL+DL+ML        Version?                        UInt32
+                //      Most of the samples have 0x00000000
+                //      lady.plj has 0x00000002 and references "ad006376_5.dat" after, version 2?
+                //      nature_soundscape_v.plj has 0x00C88028
             }
             catch (Exception ex)
             {
