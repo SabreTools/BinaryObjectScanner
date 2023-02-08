5682bd41a1a621826140389db33c75dcbbb4316e
SilasLaspada <silasqwerty@ymail.com>
2023-02-08 13:44:36 -0700

    Add new MGI Registration check
    * Add new MGI Registration check

--- BurnOutSharp/ProtectionType/MGIRegistration.cs
+++ BurnOutSharp/ProtectionType/MGIRegistration.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using BurnOutSharp.Interfaces;
 using BurnOutSharp.Wrappers;
 
@@ -14,8 +15,16 @@ namespace BurnOutSharp.ProtectionType
             if (sections == null)
                 return null;
 
+            string name = pex.ProductName;
+
+            // Found in "Register.dll" in IA item "MGIPhotoSuite4.0AndPhotoVista2.02001".
+            if (name?.Equals("MGI Registration Utility", StringComparison.Ordinal) == true)
+                return $"MGI Registration {Tools.Utilities.GetInternalVersion(pex)}";
+
             // Found in "Register.dll" from "VideoWaveIII" in IA item "mgi-videowave-iii-version-3.00-mgi-software-2000".
-            var resources = pex.FindStringTableByEntry("MGI Registration");
+            var resources = pex.FindStringTableByEntry("MGI Registration")
+            // Found in "Register.dll" in IA item "MGIPhotoSuite4.0AndPhotoVista2.02001".
+            .Concat(pex.FindStringTableByEntry("Register@register.mgisoft.com"));
             if (resources.Any())
                 return "MGI Registration";
 
