8fe5046c1922b0512117dd68f3ab5a3811c42c1e
TheRogueArchivist <silasqwerty@ymail.com>
2023-02-08 14:04:47 -0700

    Add new MGI Registration check (#232)
    * Add new MGI Registration check
    
    * Add new MGI Registration check
    
    * Address PR comment
    
    * Address PR comment

--- BurnOutSharp/ProtectionType/MGIRegistration.cs
+++ BurnOutSharp/ProtectionType/MGIRegistration.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using BurnOutSharp.Interfaces;
 using BurnOutSharp.Wrappers;
 
@@ -14,8 +15,19 @@ namespace BurnOutSharp.ProtectionType
             if (sections == null)
                 return null;
 
+            string name = pex.ProductName;
+
+            // Found in "Register.dll" in IA item "MGIPhotoSuite4.0AndPhotoVista2.02001".
+            if (name?.Equals("MGI Registration Utility", StringComparison.Ordinal) == true)
+                return $"MGI Registration {Tools.Utilities.GetInternalVersion(pex)}";
+
             // Found in "Register.dll" from "VideoWaveIII" in IA item "mgi-videowave-iii-version-3.00-mgi-software-2000".
             var resources = pex.FindStringTableByEntry("MGI Registration");
+            if (resources.Any())
+                return "MGI Registration";
+
+            // Found in "Register.dll" in IA item "MGIPhotoSuite4.0AndPhotoVista2.02001".
+            resources = pex.FindStringTableByEntry("Register@register.mgisoft.com");
             if (resources.Any())
                 return "MGI Registration";
 
