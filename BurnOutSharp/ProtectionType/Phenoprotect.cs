93c03d6a836f9cb8a4277553d6d2397941ff247d
SilasLaspada <silasqwerty@ymail.com>
2023-02-15 15:21:10 -0700

    Add DRML link to phenoProtect
    * Add DRML link to phenoProtect.
    * Fix case of the "phenoProtect" class name.

--- BurnOutSharp/ProtectionType/Phenoprotect.cs
+++ BurnOutSharp/ProtectionType/Phenoprotect.cs
@@ -1,9 +1,9 @@
 ﻿namespace BurnOutSharp.ProtectionType
 {
-    public class Phenoprotect
+    public class phenoProtect
     {
         // Currently implemented as a text file check, more checks are likely possible but currently unknown.
         // Current checks based off Redump entry 84082 are found in the InstallShield setup.inx file, but the game also checks if the original disc is present in the drive after installation as well, so it seems unlikely for the InstallShield check to be relevant at that stage.
-        // https://www.cdmediaworld.com/hardware/cdrom/cd_protections_phenoprotect.shtml
+        // <see href="https://github.com/TheRogueArchivist/DRML/blob/main/entries/phenoProtect/phenoProtect.md"/>
     }
 }
