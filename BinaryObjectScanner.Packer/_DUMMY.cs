95755b930d9337b1a19e467ca2612f8f7252498c
Matt Nadareski <mnadareski@outlook.com>
2023-03-09 23:31:45 -0500

    Add packer library skeleton

---
+++ BinaryObjectScanner.Packer/_DUMMY.cs
@@ -0,0 +1,7 @@
+﻿namespace BinaryObjectScanner.Packer
+{
+    /// <summary>
+    /// This class exists for reflection purposes and should not be used
+    /// </summary>
+    public sealed class _DUMMY { }
+}
