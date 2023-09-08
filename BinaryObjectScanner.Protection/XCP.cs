1c972a29a7160b25cd01f427f3a647faf16dec3e
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 12:19:31 -0400

    Migrate INI reading to Nuget

--- BinaryObjectScanner.Protection/XCP.cs
+++ BinaryObjectScanner.Protection/XCP.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using BinaryObjectScanner.Interfaces;
-using BinaryObjectScanner.Utilities;
 using BinaryObjectScanner.Wrappers;
+using SabreTools.IO;
 
 namespace BinaryObjectScanner.Protection
 {
