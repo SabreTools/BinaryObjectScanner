6c9cd72948a3bda6059d83a500a0b7e5a7bdc49c
TheRogueArchivist <24215969+TheRogueArchivist@users.noreply.github.com>
2024-01-26 07:40:09 -0700

    Add CD-X notes (#278)
    * Add CD-X notes
    
    * Add additional note

--- BinaryObjectScanner/Protection/CDX.cs
+++ BinaryObjectScanner/Protection/CDX.cs
@@ -7,6 +7,10 @@ using SabreTools.Matching;
 
 namespace BinaryObjectScanner.Protection
 {
+    // Nothing is currently known about this DRM. One program may possibly have included it, as it has been listed as including these files in the installion directory (https://www.advanceduninstaller.com/Visit-Gallery-2-90896afd3151ed9660dddc23b892863f-application.htm).
+    // Unfortunately, this program and developer are so obscure, I'm not able to find any relevant further information on them whatsoever.
+    // The only source of valuable information currently known is a forum post about a user attempting to crack this DRM (https://forum.p30world.com/showthread.php?t=413264).
+    // Every attachment, upload, or photo from the forum thread are offline and unarchived.
     public class CDX : IPathCheck
     {
         /// <inheritdoc/>
