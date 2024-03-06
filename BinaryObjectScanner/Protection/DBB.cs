43736b85518d9beb5227018bc246375bf9b717f8
TheRogueArchivist <24215969+TheRogueArchivist@users.noreply.github.com>
2024-03-06 09:12:27 -0700

    Remove DBB Skeleton
    DBB is now documented in DRML.

--- BinaryObjectScanner/Protection/DBB.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BinaryObjectScanner.Protection
-{
-    public class DBB
-    {
-        // TODO: Implement - http://web.archive.org/web/20040604233815/www.wkit.com/sites/wkit/setup/eng/index.asp
-    }
-}
