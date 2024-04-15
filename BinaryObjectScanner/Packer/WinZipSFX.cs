be114f60d305066a896438d1ab9822bdbb09e360
TheRogueArchivist <24215969+TheRogueArchivist@users.noreply.github.com>
2024-04-14 22:18:00 -0600

    Fix WinZip SFX folders not being scanned (#299)
    * Fix WinZip SFX folders not being scanned
    
    Use PKZIP extraction to fix WinZip SFX extraction not extracting folders.
    
    * Remove unneeded null check
    
    * Add checks for incomplete zip entries

--- BinaryObjectScanner/Packer/WinZipSFX.cs
+++ BinaryObjectScanner/Packer/WinZipSFX.cs
@@ -73,20 +73,18 @@ namespace BinaryObjectScanner.Packer
         /// <summary>
         /// Handle common extraction between executable types
         /// </summary>
-        private static string? Extract(string file, bool includeDebug)
+        /// <inheritdoc/>
+        public string? Extract(string file, bool includeDebug)
         {
 #if NET462_OR_GREATER || NETCOREAPP
             try
             {
-                // Should be using stream instead of file, but stream fails to extract anything. My guess is that the executable portion of the archive is causing stream to fail, but not file.
+                // Create a temp output directory
+                string tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                Directory.CreateDirectory(tempPath);
+
                 using (ZipArchive zipFile = ZipArchive.Open(file))
                 {
-                    if (!zipFile.IsComplete)
-                        return null;
-
-                    string tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-                    Directory.CreateDirectory(tempPath);
-
                     foreach (var entry in zipFile.Entries)
                     {
                         try
@@ -95,7 +93,14 @@ namespace BinaryObjectScanner.Packer
                             if (entry.IsDirectory)
                                 continue;
 
+                            // If we have a partial entry due to an incomplete multi-part archive, skip it
+                            if (!entry.IsComplete)
+                                continue;
+
                             string tempFile = Path.Combine(tempPath, entry.Key);
+                            var directoryName = Path.GetDirectoryName(tempFile);
+                            if (directoryName != null && !Directory.Exists(directoryName))
+                                Directory.CreateDirectory(directoryName);
                             entry.WriteToFile(tempFile);
                         }
                         catch (Exception ex)
@@ -103,9 +108,9 @@ namespace BinaryObjectScanner.Packer
                             if (includeDebug) Console.WriteLine(ex);
                         }
                     }
-
-                    return tempPath;
                 }
+
+                return tempPath;
             }
             catch (Exception ex)
             {
