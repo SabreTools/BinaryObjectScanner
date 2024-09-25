9d20820cf4258a3854ba77a5a142d364efeeab6b
TheRogueArchivist <24215969+TheRogueArchivist@users.noreply.github.com>
2024-09-25 07:31:33 -0600

    Add 7-Zip SFX extraction
    Newest SharpCompress added support for extracting 7-Zip SFX files, so we can add support for them here too.

--- BinaryObjectScanner/Packer/SevenZipSFX.cs
+++ BinaryObjectScanner/Packer/SevenZipSFX.cs
@@ -1,10 +1,17 @@
+using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using BinaryObjectScanner.Interfaces;
 using SabreTools.Serialization.Wrappers;
+#if NET462_OR_GREATER || NETCOREAPP
+using SharpCompress.Archives;
+using SharpCompress.Archives.SevenZip;
+using SharpCompress.Readers;
+#endif
 
 namespace BinaryObjectScanner.Packer
 {
-    // TODO: Add extraction
     public class SevenZipSFX : IExtractablePortableExecutable, IPortableExecutableCheck
     {
         /// <inheritdoc/>
@@ -46,8 +53,64 @@ namespace BinaryObjectScanner.Packer
 
         /// <inheritdoc/>
         public string? Extract(string file, PortableExecutable pex, bool includeDebug)
+            => Extract(file, includeDebug);
+
+        /// <inheritdoc/>
+        public string? Extract(string file, bool includeDebug)
         {
+            if (!File.Exists(file))
+                return null;
+
+            using var fs = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            return Extract(fs, file, includeDebug);
+        }
+
+        /// <inheritdoc/>
+        public string? Extract(Stream? stream, string file, bool includeDebug)
+        {
+            if (stream == null)
+                return null;
+
+#if NET462_OR_GREATER || NETCOREAPP
+            try
+            {
+                // Create a temp output directory
+                string tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                Directory.CreateDirectory(tempPath);
+                using (SevenZipArchive sevenZipFile = SevenZipArchive.Open(stream, new ReaderOptions() { LookForHeader = true }))
+                {
+                    foreach (var entry in sevenZipFile.Entries)
+                    {
+                        try
+                        {
+                            // If the entry is a directory
+                            if (entry.IsDirectory)
+                                continue;
+
+                            // If the entry has an invalid key
+                            if (entry.Key == null)
+                                continue;
+
+                            string tempFile = Path.Combine(tempPath, entry.Key);
+                            entry.WriteToFile(tempFile);
+                        }
+                        catch (Exception ex)
+                        {
+                            if (includeDebug) Console.WriteLine(ex);
+                        }
+                    }
+                }
+
+                return tempPath;
+            }
+            catch (Exception ex)
+            {
+                if (includeDebug) Console.WriteLine(ex);
+                return null;
+            }
+#else
             return null;
+#endif
         }
     }
 }
