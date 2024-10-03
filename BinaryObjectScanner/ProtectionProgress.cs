b08921a94a0557d43eed849b350820a3e581e503
Matt Nadareski <mnadareski@outlook.com>
2024-10-03 12:52:27 -0400

    Use readonly instead of private set

--- BinaryObjectScanner/ProtectionProgress.cs
+++ BinaryObjectScanner/ProtectionProgress.cs
@@ -12,17 +12,17 @@
         /// <summary>
         /// Filename to report progress for
         /// </summary>
-        public string? Filename { get; private set; }
+        public readonly string? Filename { get; }
 
         /// <summary>
         /// Value between 0 and 1 representign the percentage completed
         /// </summary>
-        public float Percentage { get; private set; }
+        public readonly float Percentage { get; }
 
         /// <summary>
         /// Protection information to report
         /// </summary>
-        public string? Protection { get; private set; }
+        public readonly string? Protection { get; }
 
         public ProtectionProgress(string? filename, float percentage, string? protection)
         {
