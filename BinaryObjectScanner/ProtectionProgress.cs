aa4c72fb90bab398190392e0dc2000c0812bf5db
Matt Nadareski <mnadareski@outlook.com>
2024-10-03 12:55:38 -0400

    Old .NET really doesn't like readonly

--- BinaryObjectScanner/ProtectionProgress.cs
+++ BinaryObjectScanner/ProtectionProgress.cs
@@ -12,17 +12,17 @@
         /// <summary>
         /// Filename to report progress for
         /// </summary>
-        public readonly string? Filename { get; }
+        public string? Filename { get; }
 
         /// <summary>
         /// Value between 0 and 1 representign the percentage completed
         /// </summary>
-        public readonly float Percentage { get; }
+        public float Percentage { get; }
 
         /// <summary>
         /// Protection information to report
         /// </summary>
-        public readonly string? Protection { get; }
+        public string? Protection { get; }
 
         public ProtectionProgress(string? filename, float percentage, string? protection)
         {
