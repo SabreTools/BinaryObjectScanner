ffca1562091a85d6ea94ae3163860f042b3d7089
Matt Nadareski <mnadareski@outlook.com>
2024-10-03 12:51:30 -0400

    Remove init variants :(

--- BinaryObjectScanner/ProtectionProgress.cs
+++ BinaryObjectScanner/ProtectionProgress.cs
@@ -12,29 +12,17 @@
         /// <summary>
         /// Filename to report progress for
         /// </summary>
-#if NETFRAMEWORK || NETCOREAPP3_1
         public string? Filename { get; private set; }
-#else
-        public string? Filename { get; init; }
-#endif
 
         /// <summary>
         /// Value between 0 and 1 representign the percentage completed
         /// </summary>
-#if NETFRAMEWORK || NETCOREAPP3_1
         public float Percentage { get; private set; }
-#else
-        public float Percentage { get; init; }
-#endif
 
         /// <summary>
         /// Protection information to report
         /// </summary>
-#if NETFRAMEWORK || NETCOREAPP3_1
         public string? Protection { get; private set; }
-#else
-        public string? Protection { get; init; }
-#endif
 
         public ProtectionProgress(string? filename, float percentage, string? protection)
         {
