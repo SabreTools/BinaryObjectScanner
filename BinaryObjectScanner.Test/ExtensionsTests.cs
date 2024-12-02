3141e1f02036c9d2350bd2c581ef0af78ecc7d4e
Matt Nadareski <mnadareski@outlook.com>
2024-12-02 11:43:04 -0500

    Add file size helper extension

--- BinaryObjectScanner.Test/ExtensionsTests.cs
+++ BinaryObjectScanner.Test/ExtensionsTests.cs
@@ -6,6 +6,26 @@ namespace BinaryObjectScanner.Test
 {
     public class ExtensionsTests
     {
+        #region FileSize
+
+        [Fact]
+        public void FileSize_Null_Invalid()
+        {
+            string? filename = null;
+            long actual = filename.FileSize();
+            Assert.Equal(-1, actual);
+        }
+
+        [Fact]
+        public void FileSize_Empty_Invalid()
+        {
+            string? filename = string.Empty;
+            long actual = filename.FileSize();
+            Assert.Equal(-1, actual);
+        }
+
+        #endregion
+
         #region IterateWithAction
 
         [Fact]
--- BinaryObjectScanner/Extensions.cs
+++ BinaryObjectScanner/Extensions.cs
@@ -1,10 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace BinaryObjectScanner
 {
     internal static class Extensions
     {
+        /// <summary>
+        /// Helper to get the filesize from a path
+        /// </summary>
+        /// <returns>Size of the file path, -1 on error</returns>
+        public static long FileSize(this string? filename)
+        {
+            // Invalid filenames are ignored
+            if (string.IsNullOrEmpty(filename))
+                return -1;
+
+            // Non-file paths are ignored
+            if (!File.Exists(filename))
+                return -1;
+
+            return new FileInfo(filename).Length;
+        }
+
         /// <summary>
         /// Wrap iterating through an enumerable with an action
         /// </summary>
