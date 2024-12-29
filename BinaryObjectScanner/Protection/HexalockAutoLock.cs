e157c71acaa6d8727f5b94cdb10aac83968f31fe
HeroponRikiBestest <jojloj3@gmail.com>
2024-12-29 14:28:55 -0500

    Add newlines between if blocks.

--- BinaryObjectScanner/Protection/HexalockAutoLock.cs
+++ BinaryObjectScanner/Protection/HexalockAutoLock.cs
@@ -57,6 +57,7 @@ namespace BinaryObjectScanner.Protection
                 if (strs.Exists(s => s.Contains("mfint.dll")))
                     return "Hexalock Autolock";
             }
+            
             // Get the code/CODE section strings, if they exist
             strs = pex.GetFirstSectionStrings("code") ?? pex.GetFirstSectionStrings("CODE");
             if (strs != null)
@@ -65,6 +66,7 @@ namespace BinaryObjectScanner.Protection
                 if (strs.Exists(s => s.Contains("mfint.dll")))
                     return "Hexalock Autolock";
             }
+            
             // Get the UPX1 section strings, if they exist
             strs = pex.GetFirstSectionStrings("UPX1");
             if (strs != null)
