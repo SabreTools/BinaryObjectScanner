967f14fc42669420cc0c031823dc65bfe3e953f0
HeroponRikiBestest <50224630+HeroponRikiBestest@users.noreply.github.com>
2025-01-13 23:03:35 -0500

    Change Crunch to use IExtractableExecutable

--- BinaryObjectScanner/Packer/Crunch.cs
+++ BinaryObjectScanner/Packer/Crunch.cs
@@ -6,7 +6,7 @@ namespace BinaryObjectScanner.Packer
     // Packer used by all known SmartE games, but also used by some other non-SmartE protected software as well.
     // https://web.archive.org/web/20020806102129/http://www.bit-arts.com/windows_solutions.html
     // TODO: Other BitArts products may also use this same string. No samples have yet been found.
-    public class Crunch : IExecutableCheck<PortableExecutable>
+    public class Crunch : IExtractableExecutable<PortableExecutable>
     {
         /// <inheritdoc/>
         public string? CheckExecutable(string file, PortableExecutable pex, bool includeDebug)
@@ -21,6 +21,12 @@ namespace BinaryObjectScanner.Packer
             }
 
             return null;
+        }
+
+        /// <inheritdoc/>
+        public bool Extract(string file, PortableExecutable pex, string outDir, bool includeDebug)
+        {
+            return false;
         }
     }
 }
