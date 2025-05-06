5e785c0879415e0163856dbac4b3233b48f1e086
HeroponRikiBestest <50224630+HeroponRikiBestest@users.noreply.github.com>
2025-05-06 15:34:21 -0400

    Fix if else

--- BinaryObjectScanner/Protection/SecuROM.cs
+++ BinaryObjectScanner/Protection/SecuROM.cs
@@ -23,7 +23,8 @@ namespace BinaryObjectScanner.Protection
             if (name.OptionalEquals("paul.dll", StringComparison.OrdinalIgnoreCase))
                 if (pex.ProductName.OptionalEquals("drEAm"))
                     return $"SecuROM Product Activation v{pex.GetInternalVersion()} - EA Game Authorization Management";
-                return $"SecuROM Product Activation v{pex.GetInternalVersion()}";
+                else
+                    return $"SecuROM Product Activation v{pex.GetInternalVersion()}";
             else if (name.OptionalEquals("paul_dll_activate_and_play.dll"))
                 return $"SecuROM Product Activation v{pex.GetInternalVersion()}";
             else if (name.OptionalEquals("paul_dll_preview_and_review.dll"))
