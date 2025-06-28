d0dad6a90cf5841bedfa9fd9f7a70e61f443f501
HeroponRikiBestest <jojloj3@gmail.com>
2025-06-28 14:26:49 -0400

    Fixes .hdr+.cab installshield cabinet files not being extracted when relative paths are provided.

--- BinaryObjectScanner/FileType/InstallShieldCAB.cs
+++ BinaryObjectScanner/FileType/InstallShieldCAB.cs
@@ -24,6 +24,7 @@ namespace BinaryObjectScanner.FileType
         /// <inheritdoc/>
         public bool Extract(Stream? stream, string file, string outDir, bool includeDebug)
         {
+            file = Path.GetFullPath(file);
             // Get the name of the first cabinet file or header
             var directory = Path.GetDirectoryName(file);
             string noExtension = Path.GetFileNameWithoutExtension(file);
