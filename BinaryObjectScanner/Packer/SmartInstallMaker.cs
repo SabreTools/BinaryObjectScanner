6e334c8770722eaa423b182f5245d97878a78333
Matt Nadareski <mnadareski@outlook.com>
2025-09-08 07:27:26 -0400

    Add Smart Install Maker detection (fixes #101)

---
+++ BinaryObjectScanner/Packer/SmartInstallMaker.cs
@@ -0,0 +1,22 @@
+using BinaryObjectScanner.Interfaces;
+using SabreTools.Serialization.Wrappers;
+
+namespace BinaryObjectScanner.Packer
+{
+    /// <summary>
+    /// Smart Install Maker
+    /// </summary>
+    public class SmartInstallMaker : IExecutableCheck<PortableExecutable>
+    {
+        /// <inheritdoc/>
+        public string? CheckExecutable(string file, PortableExecutable exe, bool includeDebug)
+        {
+            // <see href="https://www.virustotal.com/gui/file/43b5791a7d87830025efae0db8f1cc5a02b6001f4703b0189adf1412cdbe22ac/details"/>
+            var name = exe.AssemblyDescription;
+            if (name.OptionalContains("Smart Install Maker"))
+                return "Smart Install Maker";
+
+            return null;
+        }
+    }
+}
