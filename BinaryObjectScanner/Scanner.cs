799489d0790b3f95a6015d8ce7c3f418912b2ca6
Matt Nadareski <mnadareski@outlook.com>
2025-09-20 15:29:13 -0400

    Reduce unnecessary code separation

--- BinaryObjectScanner/Scanner.cs
+++ BinaryObjectScanner/Scanner.cs
@@ -303,7 +303,7 @@ namespace BinaryObjectScanner
                 #region Non-Archive File Types
 
                 // Create a detectable for the given file type
-                var detectable = Factory.CreateDetectable(fileType, wrapper);
+                var detectable = CreateDetectable(fileType, wrapper);
 
                 // If we're scanning file contents
                 if (detectable != null && _scanContents)
@@ -430,6 +430,43 @@ namespace BinaryObjectScanner
             }
 
             return protections;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Create an instance of a detectable based on file type
+        /// </summary>
+        private static IDetectable? CreateDetectable(WrapperType fileType, IWrapper? wrapper)
+        {
+            // Use the wrapper before the type
+            switch (wrapper)
+            {
+                case AACSMediaKeyBlock obj: return new FileType.AACSMediaKeyBlock(obj);
+                case BDPlusSVM obj: return new FileType.BDPlusSVM(obj);
+                // case CIA obj => new FileType.CIA(obj),
+                case LinearExecutable obj: return new FileType.LinearExecutable(obj);
+                case MSDOS obj: return new FileType.MSDOS(obj);
+                // case N3DS obj: return new FileType.N3DS(obj);
+                case NewExecutable obj: return new FileType.NewExecutable(obj);
+                case PlayJAudioFile obj: return new FileType.PLJ(obj);
+                case PortableExecutable obj: return new FileType.PortableExecutable(obj);
+            }
+
+            // Fall back on the file type for types not implemented in Serialization
+            return fileType switch
+            {
+                // WrapperType.CIA => new FileType.CIA(),
+                WrapperType.LDSCRYPT => new FileType.LDSCRYPT(),
+                // WrapperType.N3DS => new FileType.N3DS(),
+                WrapperType.RealArcadeInstaller => new FileType.RealArcadeInstaller(),
+                WrapperType.RealArcadeMezzanine => new FileType.RealArcadeMezzanine(),
+                WrapperType.SFFS => new FileType.SFFS(),
+                WrapperType.Textfile => new FileType.Textfile(),
+                _ => null,
+            };
         }
 
         #endregion
