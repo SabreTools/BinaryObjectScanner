7e7d166ed880e815d96a4d85905d6ef04ffa479e
Matt Nadareski <mnadareski@outlook.com>
2025-09-25 21:02:54 -0400

    Add note for Advanced Installer

--- BinaryObjectScanner/Packer/AdvancedInstaller.cs
+++ BinaryObjectScanner/Packer/AdvancedInstaller.cs
@@ -5,6 +5,12 @@ namespace BinaryObjectScanner.Packer
 {
     // TODO: Add extraction
     // TODO: Verify that all versions are detected
+    // Using the file "ds-1.0.6-patch.exe", it seems like the way that AdvancedInstaller
+    // works is by packing all of the files sequentially in the overlay and referencing
+    // them somehow. This has the unfortunate effect that, if any of these files are
+    // executables, then every single file appears embedded in the first's overlay.
+    // While this technically extracts the data, it does so improperly. It may require
+    // using the size of image and headers for overlay-extracted data.
     public class AdvancedInstaller : IExecutableCheck<PortableExecutable>
     {
         /// <inheritdoc/>
