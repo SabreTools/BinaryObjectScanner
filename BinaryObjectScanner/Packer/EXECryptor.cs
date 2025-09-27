ee5ed5e833be6de5a84f4f9f48913a09d44095e8
Matt Nadareski <mnadareski@outlook.com>
2025-09-26 20:51:29 -0400

    Add some notes about EXECryptor

---
+++ BinaryObjectScanner/Packer/EXECryptor.cs
@@ -0,0 +1,12 @@
+namespace BinaryObjectScanner.Packer
+{
+    // TODO: Implement
+    public class EXECryptor
+    {
+        // Multiple "fake" section names, such as:
+        // "nywteoys", "w01oolod", "af9xtrvu", akottyw9",
+        // "n6t.cpbn". Across all samples, the same names
+        // are used for the same section numbers. This
+        // may be because all samples are from the same source.
+    }
+}
