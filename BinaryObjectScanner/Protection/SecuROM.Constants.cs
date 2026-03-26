a1017138d5dd3aa1330163aad129aa960cc7e38e
HeroponRikiBestest <jojloj3@gmail.com>
2026-03-26 14:37:09 -0400

    Update RC table with newly found executables

--- BinaryObjectScanner/Protection/SecuROM.Constants.cs
+++ BinaryObjectScanner/Protection/SecuROM.Constants.cs
@@ -12,8 +12,9 @@ namespace BinaryObjectScanner.Protection
         {
             {"C6DFF6B08EE126893840E107FD4EC9F6", "Alice - Madness Returns (USA)+(Europe)"},
             {"890C4DBDB7819D1FC73FE2105CED79FA", "Alice - Madness Returns (Steam v0)"},
+            {"5D785311E052C6B9F2C4B1A0022BBEF7", "Alice - Madness Returns (Unknown Variant)"}, // Not sure where the RC executable came from
             {"D7703D32B72185358D58448B235BD55E", "Arcania - Gothic 4 (Australia)"}, // International version (English, French, Italian, German, Spanish)
-            // Arcania - Gothic 4 - Polish(/Hungarian?) - known to most likely exist. Likely matches support site exe.
+            {"D6D6F97F99EC02A9DB4FCDE841C94726", "Arcania - Gothic 4 (Poland)"}, // Possibly Poland, Hungary?
             {"83CD6225899C08422F860095962287A5", "Arcania - Gothic 4 (Russia)"},
             // Arcania - Gothic 4 - Chinese - known to most likely exist. Likely matches support site exe.
             {"FAF6DD75DDB335101CB77A714793DC28", "Batman - Arkham City - Game of the Year Edition (UK)"},
@@ -22,6 +23,7 @@ namespace BinaryObjectScanner.Protection
             {"56C23D930F885BA5BF026FEABFC31856", "Battlefield 3 (USA)+(Europe, Asia)"},
             {"631C0ACE596722488E3393BD1AFCE731", "Battlefield 3 (Russia)"},
             {"6E481CDEBDB30B8889340CEC3300C931", "Battlefield 3 (UK)"},
+            {"3963DD473C6659DB9D8F4452E6C37554", "Battlefield 3 (Unknown Variant)"},  // Not sure where the RC executable came from
             {"C5AB3931A3CBB0141CC5A4638C391F4F", "BioShock 2 (Argentina)+(Europe, Australia)+(Europe)+(Europe) (Alt)+(Netherlands)+(USA) - Multiplayer executable"},
             {"73DB35419A651CB69E78A641BBC88A4C", "BioShock 2 (Argentina)+(Europe, Australia)+(Europe)+(Europe) (Alt)+(Netherlands)+(USA) - Singleplayer executable"},
             {"E5D63D369023A1D1074E7B13952FA0F2", "BioShock 2 (Russia) - Multiplayer executable"},
@@ -39,6 +41,7 @@ namespace BinaryObjectScanner.Protection
             {"3F1AFA4783F9001AACF0379A2A432A13", "Dragon Age II (Europe)+(USA) - Mac executable"},
             {"530A3EB454570EEE5519ABE6BAE0187C", "Far Cry 2 (Europe)+(USA) (En,Fr,De,Es,It)"},
             {"4B3B130A70F3711BFA8AF06195FE4250", "FIFA 12 (Europe)"},
+            {"D079D0302824335AF1D1AB0465267948", "FIFA 12 (Unknown Variant)"}, // Not sure where the RC executable came from
             {"F43F777696B0FAD3A331298C48104B31", "FIFA 13 (Europe)"},
             {"1DF0E096068839C12E4B353AC50E41FA", "Grand Theft Auto - Episodes from Liberty City (Russia)"},
             {"F3ADC6D08BEC42FB988F2F62B5C731FA", "Grand Theft Auto - Episodes from Liberty City (USA)"},
@@ -53,6 +56,7 @@ namespace BinaryObjectScanner.Protection
             {"4EA3CE0670DECD0A74FA312714C22025", "Need for Speed - The Run (Europe)"},
             {"88AB0D4A4EE7867F740AD063400FCDB5", "Need for Speed - The Run (Russia)"},
             {"EAD8E224D0F44706BA92BD9B27FEBA7D", "Need for Speed - The Run (USA)"},
+            {"90919AAA29AC678D49FB2BEDC6B795EF", "Need for Speed - The Run (Unknown Alt)"}, // Not sure where the RC executable came from
             {"316FF217BD129F9EEBD05A321A8FBE60", "Syndicate (USA)+(Europe) (En,Fr,De,Es,It,Ru)"},
         };
 
@@ -80,6 +84,7 @@ namespace BinaryObjectScanner.Protection
             {17199339, "AliceMadnessReturns.aec"},
             {22357747, "MassEffect3.aec"},
             {23069931, "fifa.aec"},
+            {25409907, "Arcania.aec"},
             {25410419, "Arcania.aec"},
             {25823091, "Arcania.aec"},
             {27564780, "output\\BFBC2Game.aec"},
