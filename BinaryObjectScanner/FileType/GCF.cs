a727af7fac602e3497f221c5b5b26f3b908ff7b7
HeroponRikiBestest <jojloj3@gmail.com>
2026-06-26 08:39:42 -0700

    Update GCF output string for encrypted GCFs

--- BinaryObjectScanner/FileType/GCF.cs
+++ BinaryObjectScanner/FileType/GCF.cs
@@ -16,19 +16,19 @@ namespace BinaryObjectScanner.FileType
             // Filename is worth reporting, since it's descriptive and has to match the Steam2 CDN in order to work.
             string fileName = Path.GetFileName(file);
             uint depotId = _wrapper.Model.Header.CacheID;
-            uint manifestVersion = _wrapper.Model.Header.LastVersionPlayed;
+            uint depotVersion = _wrapper.Model.Header.LastVersionPlayed;
 
-            // At the moment, all samples of GCF files on redump are unencrypted. Combined with being uncertain about
-            // whether this is the best way to check whether the GCF is encrypted, this block will be left commented
-            // out until further research is done.
-            // bool encrypted = false;
-            // if (_wrapper.Files != null && _wrapper.Files.Length > 0)
-            //     encrypted = _wrapper.Files[0].Encrypted;
+            // While there is a depot-level encrypted flag on the GCF, it's unfortunately completely unreliable.
+            // Technically speaking, there are some known GCFs that only have some files encrypted. HL2 discs from
+            // 2004 have a base_source_engine.gcf (depot 200) like this. However, currently unsure of the performance
+            // impacts of looping the entirety of _wrapper.Files[] just to cover a fairly niche edge case
+            bool encrypted = false;
+            if (_wrapper.Files != null && _wrapper.Files.Length > 0)
+                encrypted = _wrapper.Files[0].Encrypted;
 
-            // string encryptedString = encrypted ? "encrypted" : "unencrypted";
-            // string returnString = $"{fileName} - {depotId} (v{manifestVersion}, {encryptedString})";
-
-            string returnString = $"{fileName} - {depotId} (v{manifestVersion})";
+            // Only note encryption status if the GCF is encrypted
+            string encryptedString = encrypted ? ", encrypted" : "";
+            string returnString = $"{fileName} - {depotId} (v{depotVersion}{encryptedString})";
             return returnString;
         }
     }
